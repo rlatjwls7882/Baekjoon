2b4a8ccf90593c620aac93ef246ac916d98739ba
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-16 00:38:42 +0900

    Bronze I

--- 29xxx/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
+++ 29xxx/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
@@ -4,10 +4,5 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     string s; cin >> s;
-
-    int a = stoi(s.substr(2, s.length()-2));
-    int b = pow(10, s.length()-2);
-    int gcdVal = gcd(a, b);
-
-    cout << "YES\n" << a/gcdVal << ' ' << b/gcdVal;
+    cout << "YES\n" << stoi(s.substr(2, s.length()-2)) << ' ' << (int)pow(10, s.length()-2);
 }
