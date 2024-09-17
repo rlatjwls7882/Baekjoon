b8778a7113c0ab9519a69e1da75c7bf7713eafcd
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-17 16:27:32 +0900

    Bronze III

---
+++ 32xxx/32305. Farmers’ Market/Farmers’ Market.cc
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int a, b, d; cin >> a >> b >> d;
+    cout << (a*b+11)/12*d;
+}
