93bbb3444d396d009a79b027652cabf3f60306f7
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-18 00:25:45 +0900

    Silver V

--- 24xxx/24912. 카드 색칠/카드 색칠.cc
+++ 24xxx/24912. 카드 색칠/카드 색칠.cc
@@ -3,43 +3,28 @@ using namespace std;
 
 int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int N; cin >> N;
-    int arr[N];
-    for(int i=0;i<N;i++) {
-        cin >> arr[i];
-    }
-
-    for(int i=0;i<N;i++) {
-        if(arr[i]==0) {
-            if(i-1<0) {
-                arr[i]=1;
-            } else if(arr[i-1]==1) {
-                if(i+1<N && arr[i+1]==2) {
-                    arr[i]=3;
-                } else {
-                    arr[i]=2;
-                }
-            } else if(arr[i-1]==2) {
-                if(i+1<N && arr[i+1]==1) {
-                    arr[i]=3;
-                } else {
-                    arr[i]=1;
-                }
-            } else {
-                if(i+1<N && arr[i+1]==1) {
-                    arr[i]=2;
-                } else {
-                    arr[i]=1;
-                }
-            }
-        }
-        if(i-1>=0 && arr[i-1]==arr[i]) {
+    int n; cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin >> arr[i];
+    for(int i=0;i<n-1;i++) {
+        if(arr[i]!=0 && arr[i]==arr[i+1]) {
             cout << -1;
             return 0;
         }
     }
 
-    for(int i=0;i<N;i++) {
-        cout << arr[i] << ' ';
+    for(int i=0;i<n;i++) {
+        if(arr[i]==0) {
+            bool chk[3] = {false, };
+            if(i-1>=0) chk[arr[i-1]-1]=true;
+            if(i+1<n && arr[i+1]!=0) chk[arr[i+1]-1]=true;
+            for(int j=0;j<3;j++) {
+                if(!chk[j]) {
+                    arr[i] = j+1;
+                    break;
+                }
+            }
+        }
     }
+    for(int i=0;i<n;i++) cout << arr[i] << ' ';
 }
