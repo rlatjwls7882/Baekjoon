a675c7d186b68c60274b2127b8e74e8c0fc55861
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-18 14:05:02 +0900

    Bronze V

---
+++ 32xxx/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int r, g, b; cin >> r >> g >> b;
+    cout << r*3+g*4+b*5;
+}
