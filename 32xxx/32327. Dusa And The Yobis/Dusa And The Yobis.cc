24a1c3f1b578d4f95c5ddd684d90fefa1d396cea
Seojin Kim <107532530+rlatjwls7882@users.noreply.github.com>
2024-09-18 14:09:39 +0900

    Bronze IV

---
+++ 32xxx/32327. Dusa And The Yobis/Dusa And The Yobis.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int d; cin >> d;
+    while(true) {
+        int yobi; cin >> yobi;
+        if(cin.fail() || d<=yobi) break;
+        d += yobi;
+    }
+    cout << d;
+}
