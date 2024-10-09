a12edd2536f4e9fe6ccd3a91b6009169c2a3383d
Seojin Kim <rlatjwls7882@gmail.com>
2024-10-10 00:53:26 +0900

    Silver I

--- 02xxx/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
+++ 02xxx/2841. 외계인의 기타 연주/외계인의 기타 연주.cc
@@ -5,16 +5,16 @@ int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n, p; cin >> n >> p;
 
-    priority_queue<int> pq[6];
+    vector<stack<int>> stk(6);
     int cnt=0;
     while(n-->0) {
         int a, b; cin >> a >> b;
-        while(!pq[a-1].empty() && pq[a-1].top()>b) {
-            pq[a-1].pop();
+        while(!stk[a-1].empty() && stk[a-1].top()>b) {
+            stk[a-1].pop();
             cnt++;
         }
-        if(pq[a-1].empty() || pq[a-1].top() != b) {
-            pq[a-1].push(b);
+        if(stk[a-1].empty() || stk[a-1].top() != b) {
+            stk[a-1].push(b);
             cnt++;
         }
     }
