7dd35a60b2c13f87df992608cbdcab6b5b115698
Seojin Kim <rlatjwls7882@gmail.com>
2024-12-10 23:32:29 +0900

    Bronze II

--- 32xxx/32751. 햄버거/햄버거.cc
+++ 32xxx/32751. 햄버거/햄버거.cc
@@ -3,25 +3,16 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int n, arr[4]; string s; cin >> n;
+    int n; cin >> n;
+    int arr[4];
     for(int i=0;i<4;i++) cin >> arr[i];
-    cin >> s;
-    for(int i=0;i<s.length();i++) arr[s[i]-'a']--;
-    for(int i=0;i<4;i++) {
-        if(arr[i]<0) {
-            cout << "No";
-            return 0;
-        }
-    }
-    if(s[0]!='a' || s[s.length()-1]!='a') {
-        cout << "No";
-        return 0;
-    }
-    for(int i=1;i<s.length();i++) {
-        if(s[i]==s[i-1]) {
-            cout << "No";
-            return 0;
-        }
-    }
-    cout << "Yes";
+    string s; cin >> s;
+    
+    bool good=true;
+    for(int i=0;i<s.length();i++) if(--arr[s[i]-'a']<0) good=false;
+    if(s[0]!='a' || s[s.length()-1]!='a') good=false;
+    for(int i=1;i<s.length();i++) if(s[i]==s[i-1]) good=false;
+
+    if(good) cout << "Yes";
+    else cout << "No";
 }
