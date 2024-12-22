3f9902b146e7a377889eedec1d383b0278ce1390
Seojin Kim <rlatjwls7882@gmail.com>
2024-12-22 19:19:28 +0900

    Gold V

--- 30xxx/30013. 돌베어 법칙/돌베어 법칙.cc
+++ 30xxx/30013. 돌베어 법칙/돌베어 법칙.cc
@@ -1,26 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool chk[2000];
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int N; string s; cin >> N >> s;
-    vector<int> idx;
-    for(int i=0;i<N;i++) if(s[i]=='#') idx.push_back(i);
 
     int minCnt = N;
     for(int d=1;d<N;d++) {
-        memset(chk, false, sizeof(chk));
         int curCnt=0;
-        for(int i=0;i<idx.size();i++) {
-            int cur = idx[i];
-            if(chk[cur]) continue;
-            curCnt++;
-            while(cur<N && s[cur]=='#') {
-                chk[cur] = true;
-                cur += d;
-            }
+        for(int i=0;i<N;i++) {
+            if(s[i]=='#' && (i-d<0 || s[i-d]=='.')) curCnt++;
         }
         minCnt = min(minCnt, curCnt);
     }
