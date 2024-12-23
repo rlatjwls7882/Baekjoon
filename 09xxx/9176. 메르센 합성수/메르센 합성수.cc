ec9a447e694c66b4f2a2c6b950e39e327bf67e17
Seojin Kim <rlatjwls7882@gmail.com>
2024-12-23 15:46:05 +0900

    Update 메르센 합성수.cc

--- 09xxx/9176. 메르센 합성수/메르센 합성수.cc
+++ 09xxx/9176. 메르센 합성수/메르센 합성수.cc
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// bool isSosu(long long n) {
+//     for(long long i=2;i*i<=n;i++) {
+//         if(n%i==0) return false;
+//     }
+//     return true;
+// }
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int K; cin >> K;
@@ -13,4 +20,23 @@ int main() {
     if(47<=K) cout << "2351 * 4513 * 13264529 = 140737488355327 = ( 2 ^ 47 ) - 1\n";
     if(53<=K) cout << "6361 * 69431 * 20394401 = 9007199254740991 = ( 2 ^ 53 ) - 1\n";
     if(59<=K) cout << "179951 * 3203431780337 = 576460752303423487 = ( 2 ^ 59 ) - 1\n";
+    // for(long long i=1;i<=62;i++) {
+    //     if(!isSosu(i)) continue;
+    //     long long cur = powl(2, i)-1;
+    //     if(!isSosu(cur)) {
+    //         int cnt=0;
+    //         long long tmp = cur;
+    //         long long j=2;
+    //         for(;j*j<=cur;j++) {
+    //             while(tmp%j==0) {
+    //                 if(cnt) cout << "* "; 
+    //                 cout << j << ' ';
+    //                 tmp /= j;
+    //                 cnt++;
+    //             }
+    //         }
+    //         if(tmp!=1) cout << "* " << tmp << ' ';
+    //         cout << "= " << cur << " = ( 2 ^ " << i << " ) - 1\n";
+    //     }
+    // }
 }
