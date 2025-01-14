a0713cc3de2de5afb20995a279f92554758e026f
Seojin Kim <rlatjwls7882@gmail.com>
2025-01-14 21:34:07 +0900

    Gold V

--- 01xxx/1756. 피자 굽기/피자 굽기.cc
+++ 01xxx/1756. 피자 굽기/피자 굽기.cc
@@ -1,28 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int oven[300000], minSize[300000];
+int oven[300000];
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int D, N; cin >> D >> N;
     for(int i=0;i<D;i++) {
         cin >> oven[i];
-        minSize[i] = oven[i]; // 0~i까지의 최소 지름
-        if(i>=1) minSize[i] = min(minSize[i], minSize[i-1]);
+        if(i>=1) oven[i] = min(oven[i], oven[i-1]); // 0~i까지의 최소 지름
     }
 
     int last=D-1;
     while(N-->0) {
         int pizza; cin >> pizza;
-        while(last>=0 && pizza>minSize[last]) {
+        while(last>=0 && pizza>oven[last]) {
             last--;
         }
         if(last<0) {
             cout << 0;
             return 0;
         }
-        minSize[last]=0;
+        oven[last]=0;
     }
     cout << last+1;
 }
