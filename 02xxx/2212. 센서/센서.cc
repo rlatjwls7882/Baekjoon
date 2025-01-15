19efbc10bea5d0acb1f9df2ec0141e8444ae9058
Seojin Kim <rlatjwls7882@gmail.com>
2025-01-15 14:26:09 +0900

    Gold V

--- 02xxx/2212. 센서/센서.cc
+++ 02xxx/2212. 센서/센서.cc
@@ -1,40 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool sensor[2000001];
-
-struct line {
-    int x, y;
-};
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int N, K; cin >> N >> K;
-    set<int> sensor;
-    for(int i=0;i<N;i++) {
-        int pos; cin >> pos;
-        sensor.insert(pos);
-    }
+    vector<int> sensor(N);
+    for(int i=0;i<N;i++) cin >> sensor[i];
+    sort(sensor.begin(), sensor.end());
 
-    vector<line> v;
-    for(int pos:sensor) {
-        v.push_back({pos, pos});
+    vector<int> diff;
+    for(int i=0;i<N-1;i++) {
+        diff.push_back(sensor[i+1]-sensor[i]);
     }
-
-    while(v.size()>K) {
-        int idx=0;
-        for(int i=1;i<v.size()-1;i++) {
-            if(v[idx+1].x-v[idx].y > v[i+1].x-v[i].y) {
-                idx=i;
-            }
-        }
-        v[idx].y = v[idx+1].y;
-        v.erase(v.begin()+idx+1);
-    }
+    sort(diff.begin(), diff.end(), greater<int>());
 
     int sum=0;
-    for(int i=0;i<v.size();i++) {
-        sum += v[i].y-v[i].x;
+    for(int i=K-1;i<diff.size();i++) {
+        sum += diff[i];
     }
     cout << sum;
 }
