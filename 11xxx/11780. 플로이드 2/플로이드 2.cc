c18703ad1b0ee2203d36317c1c772f92c366606c
Seojin Kim <rlatjwls7882@gmail.com>
2025-01-20 23:21:13 +0900

    Gold II

--- 11xxx/11780. 플로이드 2/플로이드 2.cc
+++ 11xxx/11780. 플로이드 2/플로이드 2.cc
@@ -1,20 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int cost[101][101];
-vector<int> path[101][101];
+int cost[101][101], pa[101][101];
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n, m; cin >> n >> m;
     memset(cost, -1, sizeof cost);
+    memset(pa, -1, sizeof pa);
 
     while(m-->0) {
         int u, v, c; cin >> u >> v >> c;
         if(cost[u][v]==-1 || cost[u][v]>c) {
             cost[u][v]=c;
-            path[u][v].clear();
-            path[u][v].push_back(v);
+            pa[u][v]=v;
         }
     }
 
@@ -23,9 +22,7 @@ int main() {
             for(int e=1;e<=n;e++) {
                 if(s!=e && cost[s][m]!=-1 && cost[m][e]!=-1 && (cost[s][e]==-1 || cost[s][m]+cost[m][e]<cost[s][e])) {
                     cost[s][e] = cost[s][m]+cost[m][e];
-                    path[s][e].clear();
-                    for(int el:path[s][m]) path[s][e].push_back(el);
-                    for(int el:path[m][e]) path[s][e].push_back(el);
+                    pa[s][e] = pa[s][m];
                 }
             }
         }
@@ -39,12 +36,17 @@ int main() {
     }
     for(int i=1;i<=n;i++) {
         for(int j=1;j<=n;j++) {
-            if(path[i][j].size()) {
-                cout << path[i][j].size()+1 << ' ' << i;
-                for(int e:path[i][j]) cout << ' ' << e;
-                cout << '\n';
-            } else {
+            if(cost[i][j]==-1) {
                 cout << "0\n";
+            } else {
+                vector<int> res;
+                for(int k=i;;k=pa[k][j]) {
+                    res.push_back(k);
+                    if(k==j) break;
+                }
+                cout << res.size();
+                for(int e:res) cout << ' ' << e;
+                cout << '\n';
             }
         }
     }
