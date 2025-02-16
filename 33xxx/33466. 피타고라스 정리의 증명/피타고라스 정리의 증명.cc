3a73c02dfd429fc3517d00003a20323089b45e9b
Seojin Kim <rlatjwls7882@gmail.com>
2025-02-17 08:42:28 +0900

    Silver I

---
+++ 33xxx/33466. 피타고라스 정리의 증명/피타고라스 정리의 증명.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int t; cin >> t;
+    while(t-->0) {
+        long long n; cin >> n;
+        cout << n/2*2 + n << '\n';
+    }
+}
