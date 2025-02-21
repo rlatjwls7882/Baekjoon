ab0aaa2263c64b1fef1395fc32512122d7751dbc
Seojin Kim <rlatjwls7882@gmail.com>
2025-02-21 10:11:44 +0900

    Bronze II

---
+++ 33xxx/33488. 아름다운 수열/아름다운 수열.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << "YES\n";
+        for(int i=1;i<=n;i++) cout << i << ' ';
+        cout << '\n';
+    }
+}
