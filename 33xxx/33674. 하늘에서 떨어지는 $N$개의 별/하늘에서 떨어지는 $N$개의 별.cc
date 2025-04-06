73b71f675e786b03b9e2c7c5cc99f77e447fa3d4
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-07 01:58:46 +0900

    Bronze II

--- 33xxx/33674. 하늘에서 떨어지는 $N$개의 별/하늘에서 떨어지는 $N$개의 별.cc
+++ 33xxx/33674. 하늘에서 떨어지는 $N$개의 별/하늘에서 떨어지는 $N$개의 별.cc
@@ -1,24 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int s[100], cur[100];
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int N,D,K; cin >> N >> D >> K;
+    int N, D, K; cin >> N >> D >> K;
     
-    for(int i=0;i<N;i++) cin >> s[i];
+    int maxS=0;
+    for(int i=0;i<N;i++) {
+        int s; cin >> s;
+        maxS = max(maxS, s);
+    }
     
-    int clearCnt=0;
+    int clearCnt=0, cur=0;
     while(D--) {
-        for(int i=0;i<N;i++) {
-            cur[i] += s[i];
-            if(cur[i]>K) {
-                for(int i=0;i<N;i++) cur[i] = s[i];
-                clearCnt++;
-                break;
-            }
+        if(cur+maxS>K) {
+            clearCnt++;
+            cur=0;
         }
+        cur += maxS;
     }
     cout << clearCnt;
 }
