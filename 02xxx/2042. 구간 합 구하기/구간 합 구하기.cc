a977d250a2bf02b7106cdc69fc8d81e451f01a3f
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-19 17:54:15 +0900

    Gold I

--- 02xxx/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 02xxx/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,11 +1,10 @@
 #include<bits/stdc++.h>
-#define MAX 3000001
-
 using namespace std;
-typedef long long ll;
 
+typedef long long ll;
+const int MAX = 3'000'001;
 int _size=1;
-ll arr[MAX] = {0, };
+ll arr[MAX];
 
 ll sum(int L, int R, int nodeNum, int nodeL, int nodeR) {
     if(R<nodeL || nodeR<L) return 0;
@@ -23,28 +22,21 @@ void update(int nodeNum, ll val) {
     }
 }
 
-void construct() {
+void init(int N) {
+    while(_size<N) _size<<=1;
+    _size<<=1;
+    
+    for(int i=_size/2;i<_size/2+N;i++) cin >> arr[i];
+    
     for(int nodeNum=_size/2-1;nodeNum>0;nodeNum--) {
         arr[nodeNum]=arr[nodeNum*2]+arr[nodeNum*2+1];
     }
 }
 
-void calcSize(int N) {
-    while(_size<N) {
-        _size*=2;
-    }
-    _size*=2;
-}
-
 int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int N, M, K; cin >> N >> M >> K;
-    calcSize(N);
-
-    for(int i=_size/2;i<_size/2+N;i++) {
-        cin >> arr[i];
-    }
-    construct();
+    init(N);
 
     for(int i=0;i<M+K;i++) {
         ll a, b, c; cin >> a >> b >> c;
