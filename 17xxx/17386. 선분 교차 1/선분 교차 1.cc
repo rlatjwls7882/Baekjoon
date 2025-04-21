5f52197864951d8a2995a3155055966575b01349
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-21 22:46:12 +0900

    Gold III

--- 17xxx/17386. 선분 교차 1/선분 교차 1.cc
+++ 17xxx/17386. 선분 교차 1/선분 교차 1.cc
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct vec {
-    long long x, y;
+typedef long long ll;
+
+struct line {
+    ll x1, y1, x2, y2;
 };
 
-int cross(vec v1, vec v2) {
-    long long ret = v1.x*v2.y-v2.x*v1.y;
-    if(ret<0) return -1;
-    if(ret>0) return 1;
-    return 0;
+struct vec {
+    ll x, y;
+};
+
+bool isInterSect(line a, line b) {
+    vec vector1 = {a.x2-a.x1, a.y2-a.y1};
+    vec vector2 = {b.x1-a.x2, b.y1-a.y2};
+    vec vector3 = {b.x2-a.x2, b.y2-a.y2};
+
+    ll ret1 = vector1.x*vector2.y - vector2.x*vector1.y;
+    ll ret2 = vector1.x*vector3.y - vector3.x*vector1.y;
+    return ret1<0 && ret2>0 || ret1>0 && ret2<0;
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    long long x1, y1, x2, y2, x3, y3, x4, y4; cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-    cout << (cross({x2-x1, y2-y1}, {x3-x2, y3-y2})*cross({x2-x1, y2-y1}, {x4-x2, y4-y2})==-1 && cross({x4-x3, y4-y3}, {x1-x4, y1-y4})*cross({x4-x3, y4-y3}, {x2-x4, y2-y4})==-1);
+    line a, b; cin >> a.x1 >> a.y1 >> a.x2 >> a.y2 >> b.x1 >> b.y1 >> b.x2 >> b.y2;
+    cout << (isInterSect(a, b) && isInterSect(b, a));
 }
