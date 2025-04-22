f709042b1a2aab276dec4bcda5fe4416316bc2c9
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-22 13:15:07 +0900

    Platinum IV

--- 11xxx/11280. 2－SAT － 3/2－SAT － 3.cc
+++ 11xxx/11280. 2－SAT － 3/2－SAT － 3.cc
@@ -1,75 +1,60 @@
+/** https://www.acmicpc.net/problem/11280 제출 코드 */
 #include<bits/stdc++.h>
-#define MAX 20001
-
 using namespace std;
 
-int idx=1;
-int parent[MAX] = {0, };
-bool visited[MAX] = {false, };
-vector<vector<int>> connect = vector<vector<int>>(MAX);
-vector<vector<int>> SCCs;
+const int MAX = 20000;
+
+int idx, parent[MAX];
+bool visited[MAX];
+vector<vector<int>> conn(MAX), SCCs;
 stack<int> stk;
 
 int dfs(int cur) {
     stk.push(cur);
-    int remember=idx;
-    parent[cur]=idx++;
+    int rem = parent[cur] = ++idx;
 
-    for(int next:connect[cur]) {
-        if(parent[next]==0) {
-            remember = min(remember, dfs(next));
-        } else if(!visited[next]) {
-            remember = min(remember, parent[next]);
-        }
+    for(int next:conn[cur]) {
+        if(!parent[next]) rem = min(rem, dfs(next));
+        else if(!visited[next]) rem = min(rem, parent[next]);
     }
 
-    if(parent[cur]==remember) {
+    if(rem == parent[cur]) {
         SCCs.push_back(vector<int>());
         while(true) {
             int top = stk.top(); stk.pop();
             SCCs.back().push_back(top);
-            visited[top]=true;
-            parent[top]=remember;
-            if(top==cur) {
-                break;
-            }
+            visited[top] = true;
+            parent[top] = rem;
+            if(top==cur) break;
         }
     }
-    return remember;
+    return rem;
 }
 
 int oppo(int x) {
-    return x%2==0?x-1:x+1;
+    return x%2==0 ? x+1 : x-1;
 }
 
-int main(void) {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int N, M; cin >> N >> M;
-    while(M-->0) {
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m; cin >> n >> m;
+    while(m--) {
         int x, y; cin >> x >> y;
-        x = x<0 ? -x*2-1:x*2;
-        y = y<0 ? -y*2-1:y*2;
-        connect[oppo(x)].push_back(y);
-        connect[oppo(y)].push_back(x);
+        x = x<0 ? -x*2-2 : x*2-1;
+        y = y<0 ? -y*2-2 : y*2-1;
+        conn[oppo(x)].push_back(y);
+        conn[oppo(y)].push_back(x);
     }
 
-    for(int i=1;i<=N*2;i++) {
-        if(!visited[i]) {
-            dfs(i);
-        }
+    for(int i=0;i<2*n;i++) {
+        if(!visited[i]) dfs(i);
     }
 
-    bool chk=true;
-    for(int i=1;i<=N*2;i+=2) {
+    for(int i=0;i<2*n;i+=2) {
         if(parent[i]==parent[i+1]) {
-            chk=false;
-            break;
+            cout << 0;
+            return 0;
         }
     }
-
-    if(chk) {
-        cout << 1;
-    } else {
-        cout << 0;
-    }
+    cout << 1;
 }
