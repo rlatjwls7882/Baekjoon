930f848e354f89eb0ab5033366cee8037d4d6029
Seojin Kim <rlatjwls7882@gmail.com>
2025-04-25 21:40:13 +0900

    Platinum V

---
+++ 15xxx/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cout << "1999\n";
+    for(int i=0;i<999;i++) cout << "1 ";
+    for(int i=0;i<1000;i++) cout << "1000 ";
+}
