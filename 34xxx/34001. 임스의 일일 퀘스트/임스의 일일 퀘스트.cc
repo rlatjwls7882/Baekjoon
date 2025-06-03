13b961fb2e4e70464ad7b9f38479782115753fae
Seojin Kim <rlatjwls7882@gmail.com>
2025-06-03 21:14:39 +0900

    Bronze III

---
+++ 34xxx/34001. 임스의 일일 퀘스트/임스의 일일 퀘스트.cc
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int l; cin >> l;
+    if(l>=220) cout << "100 ";
+    else if(l>=210) cout << "300 ";
+    else if(l>=200) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=225) cout << "100 ";
+    else if(l>=220) cout << "300 ";
+    else if(l>=210) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=230) cout << "100 ";
+    else if(l>=225) cout << "300 ";
+    else if(l>=220) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=235) cout << "100 ";
+    else if(l>=230) cout << "300 ";
+    else if(l>=225) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=245) cout << "100 ";
+    else if(l>=235) cout << "300 ";
+    else if(l>=230) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=250) cout << "100\n";
+    else if(l>=245) cout << "300\n";
+    else if(l>=235) cout << "500\n";
+    else cout << "0\n";
+
+    if(l>=270) cout << "100 ";
+    else if(l>=265) cout << "300 ";
+    else if(l>=260) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=275) cout << "100 ";
+    else if(l>=270) cout << "300 ";
+    else if(l>=265) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=280) cout << "100 ";
+    else if(l>=275) cout << "300 ";
+    else if(l>=270) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=285) cout << "100 ";
+    else if(l>=280) cout << "300 ";
+    else if(l>=275) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=290) cout << "100 ";
+    else if(l>=285) cout << "300 ";
+    else if(l>=280) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=295) cout << "100 ";
+    else if(l>=290) cout << "300 ";
+    else if(l>=285) cout << "500 ";
+    else cout << "0 ";
+
+    if(l>=300) cout << "100 ";
+    else if(l>=295) cout << "300 ";
+    else if(l>=290) cout << "500 ";
+    else cout << "0 ";
+}
