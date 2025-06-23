6d4c83a5dd6ddb97cfb7074e03a41110c8a083d4
Seojin Kim <rlatjwls7882@gmail.com>
2025-06-23 17:52:33 +0900

    Gold V

--- 12xxx/12865. 평범한 배낭/평범한 배낭.cc
+++ 12xxx/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,34 +1,25 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(void) {
-    ios::sync_with_stdio; cin.tie(); cout.tie();
-    int N, K; cin >> N >> K;
+const int MAX = 100'001;
+const int INF = -0x3f3f3f3f;
 
-    int WV[N+1][2];
-    for(int i=1;i<=N;i++) {
-        for(int j=0;j<2;j++) {
-            cin >> WV[i][j];
+int dp[MAX];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, k; cin >> n >> k;
+
+    fill(dp, dp+MAX, INF);
+    dp[0]=0;
+    for(int i=0;i<n;i++) {
+        int w, v; cin >> w >> v;
+        for(int i=k;i-w>=0;i--) {
+            dp[i] = max(dp[i], dp[i-w]+v);
         }
     }
 
-    int dp[N+1][K+1];
-    for(int i=0;i<=N;i++) {
-        dp[i][0]=0;
-    }
-    for(int i=0;i<=K;i++) {
-        dp[0][i]=0;
-    }
-
-    // dp[i][j]: i번째 물건을 (선택/선택x) 일때 무게가 j보다 작거나 같은 최대 가치
-    for(int i=1;i<=N;i++) {
-        for(int j=1;j<=K;j++) {
-            dp[i][j] = dp[i-1][j];
-            if(j-WV[i][0]>=0) {
-                dp[i][j] = max(dp[i][j], dp[i-1][j-WV[i][0]]+WV[i][1]);
-            }
-        }
-    }
-    cout << dp[N][K];
+    int res=0;
+    for(int i=0;i<=k;i++) res = max(res, dp[i]);
+    cout << res;
 }
