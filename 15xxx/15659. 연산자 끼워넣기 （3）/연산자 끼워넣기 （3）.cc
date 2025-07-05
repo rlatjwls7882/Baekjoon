53553eaad73dd9d61929690a6687e3322e80b2b6
Seojin Kim <rlatjwls7882@gmail.com>
2025-07-05 23:22:38 +0900

    Gold IV

--- 15xxx/15659. 연산자 끼워넣기 （3）/연산자 끼워넣기 （3）.cc
+++ 15xxx/15659. 연산자 끼워넣기 （3）/연산자 끼워넣기 （3）.cc
@@ -3,20 +3,20 @@ using namespace std;
 
 int MAX = -0x3f3f3f3f;
 int MIN = 0x3f3f3f3f;
-int n, a[11], p, s, m, d;
+int n, a[11], opt[4];
 
-int opt[11];
+int visited[11];
 
 void dfs(int depth) {
     if(depth==n) {
         int sum=0, last=a[0];
         for(int i=1;i<n;i++) {
-            if(opt[i]>=3) {
-                if(opt[i]==3) last *= a[i];
+            if(visited[i]>=3) {
+                if(visited[i]==3) last *= a[i];
                 else last /= a[i];
             } else {
                 sum += last;
-                if(opt[i]==1) last = a[i];
+                if(visited[i]==1) last = a[i];
                 else last = -a[i];
             }
         }
@@ -26,29 +26,13 @@ void dfs(int depth) {
         MIN = min(MIN, sum);
         return;
     }
-    if(p) {
-        p--;
-        opt[depth]=1;
-        dfs(depth+1);
-        p++;
-    }
-    if(s) {
-        s--;
-        opt[depth]=2;
-        dfs(depth+1);
-        s++;
-    }
-    if(m) {
-        m--;
-        opt[depth]=3;
-        dfs(depth+1);
-        m++;
-    }
-    if(d) {
-        d--;
-        opt[depth]=4;
-        dfs(depth+1);
-        d++;
+    for(int i=0;i<4;i++) {
+        if(opt[i]) {
+            opt[i]--;
+            visited[depth]=i+1;
+            dfs(depth+1);
+            opt[i]++;
+        }
     }
 }
 
@@ -56,7 +40,7 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> n;
     for(int i=0;i<n;i++) cin >> a[i];
-    cin >> p >> s >> m >> d;
+    for(int i=0;i<4;i++) cin >> opt[i];
     dfs(1);
     cout << MAX << '\n' << MIN;
 }
