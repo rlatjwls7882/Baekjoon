09b6a5367ffd1fdd9985ae89f6a4f7c6cb65370f
Seojin Kim <rlatjwls7882@gmail.com>
2025-07-28 11:57:54 +0900

    Platinum V

---
+++ 14xxx/14252. 공약수열/공약수열.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++) cin >> v[i];
+    sort(v.begin(), v.end());
+
+    int cnt=0;
+    for(int i=0;i<v.size()-1;i++) {
+        if(gcd(v[i], v[i+1])!=1) {
+            int notGcd=2, minVal = v[i]+1;
+            for(int j=v[i]+1;j<v[i+1];j++) {
+                int curNotGcd = (gcd(v[i], j)!=1) + (gcd(j, v[i+1])!=1);
+                if(notGcd>curNotGcd) {
+                    notGcd = curNotGcd;
+                    minVal = j;
+                }
+            }
+            cnt++; // 최적 해 넣기
+            v.insert(v.begin()+i+1, minVal);
+        }
+    }
+    cout << cnt;
+}
