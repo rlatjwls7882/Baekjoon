2b22adfa5a541bf401371fbd1fba0a1cc151d327
Seojin Kim <rlatjwls7882@gmail.com>
2025-07-31 18:39:37 +0900

    Platinum V

--- 02xxx/2162. 선분 그룹/선분 그룹.cc
+++ 02xxx/2162. 선분 그룹/선분 그룹.cc
@@ -17,10 +17,12 @@ bool isIntersect(line a, line b) {
     int aToB2 = ccw({a.x2-a.x1, a.y2-a.y1, b.x2-a.x2, b.y2-a.y2});
     int bToA1 = ccw({b.x2-b.x1, b.y2-b.y1, a.x1-b.x2, a.y1-b.y2});
     int bToA2 = ccw({b.x2-b.x1, b.y2-b.y1, a.x2-b.x2, a.y2-b.y2});
-    return aToB1==0 && aToB2==0 && bToA1==0 && bToA2==0
-    && (min(a.x1, a.x2)<=b.x1 && b.x1<=max(a.x1, a.x2) || min(a.x1, a.x2)<=b.x2 && b.x2<=max(a.x1, a.x2) || min(b.x1, b.x2)<=a.x1 && a.x1<=max(b.x1, b.x2) || min(b.x1, b.x2)<=a.x2 && a.x2<=max(b.x1, b.x2))
-    && (min(a.y1, a.y2)<=b.y1 && b.y1<=max(a.y1, a.y2) || min(a.y1, a.y2)<=b.y2 && b.y2<=max(a.y1, a.y2) || min(b.y1, b.y2)<=a.y1 && a.y1<=max(b.y1, b.y2) || min(b.y1, b.y2)<=a.y2 && a.y2<=max(b.y1, b.y2))
-    || !(aToB1==0 && aToB2==0 && bToA1==0 && bToA2==0) && aToB1 * aToB2 <= 0 && bToA1 * bToA2 <= 0;
+
+    if(aToB1 * aToB2==0 && bToA1 * bToA2==0) {
+        return (min(a.x1, a.x2)<=b.x1 && b.x1<=max(a.x1, a.x2) || min(a.x1, a.x2)<=b.x2 && b.x2<=max(a.x1, a.x2) || min(b.x1, b.x2)<=a.x1 && a.x1<=max(b.x1, b.x2) || min(b.x1, b.x2)<=a.x2 && a.x2<=max(b.x1, b.x2))
+        && (min(a.y1, a.y2)<=b.y1 && b.y1<=max(a.y1, a.y2) || min(a.y1, a.y2)<=b.y2 && b.y2<=max(a.y1, a.y2) || min(b.y1, b.y2)<=a.y1 && a.y1<=max(b.y1, b.y2) || min(b.y1, b.y2)<=a.y2 && a.y2<=max(b.y1, b.y2));
+    }
+    return aToB1 * aToB2 <= 0 && bToA1 * bToA2 <= 0;
 }
 
 const int MAX = 3000;
