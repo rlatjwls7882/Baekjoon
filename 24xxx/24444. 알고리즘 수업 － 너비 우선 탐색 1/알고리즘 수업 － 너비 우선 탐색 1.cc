c912b0a4983739860f559305fe8307bdb330c2b8
Seojin Kim <rlatjwls7882@gmail.com>
2025-08-14 01:19:07 +0900

    Silver II

---
+++ 24xxx/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX = 200'001;
+
+int cnt, visited[MAX];
+vector<vector<int>> conn(MAX);
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m, r; cin >> n >> m >> r;
+    while(m--) {
+        int u, v; cin >> u >> v;
+        conn[u].push_back(v);
+        conn[v].push_back(u);
+    }
+    for(int i=1;i<=n;i++) sort(conn[i].begin(), conn[i].end());
+
+    queue<int> q; q.push(r);
+    visited[r] = ++cnt;
+    while(!q.empty()) {
+        int cur = q.front(); q.pop();
+        for(int next: conn[cur]) {
+            if(!visited[next]) {
+                visited[next] = ++cnt;
+                q.push(next);
+            }
+        }
+    }
+    for(int i=1;i<=n;i++) cout << visited[i] << '\n';
+}
