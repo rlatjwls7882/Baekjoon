7011057c5225459914f853ac30fb997fcc8e7750
Seojin Kim <rlatjwls7882@gmail.com>
2025-08-15 07:12:07 +0900

    Platinum IV

--- 05xxx/5541. 釘 （Nails）/釘 （Nails）.cc
+++ 05xxx/5541. 釘 （Nails）/釘 （Nails）.cc
@@ -1,46 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int m[5002][5002] = {0, };
+const int MAX = 5002;
+
+int board[MAX][MAX];
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int N, M; cin >> N >> M;
-    while(M-->0) {
-        int a, b, c; cin >> a >> b >> c;
-        m[a-1][b-1]++;
-        m[a-1][b]--;
-        m[a+c][b-1]--;
-        m[a+c][b+c+1]++;
-        m[a+c+1][b]++;
-        m[a+c+1][b+c+1]--;
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m; cin >> n >> m;
+    while(m--) {
+        int a, b, x; cin >> a >> b >> x;
+        board[a-1][b-1]++;
+        board[a-1][b]--;
+        board[a+x][b-1]--;
+        board[a+x][b+x+1]++;
+        board[a+x+1][b]++;
+        board[a+x+1][b+x+1]--;
     }
 
     // 오른쪽
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            m[i][j+1] += m[i][j];
+    for(int i=0;i<=n;i++) {
+        for(int j=0;j<=n;j++) {
+            board[i][j+1] += board[i][j];
         }
     }
 
-    // 아래
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            m[i+1][j] += m[i][j];
+    // 아래쪽
+    for(int i=0;i<=n;i++) {
+        for(int j=0;j<=n;j++) {
+            board[i+1][j] += board[i][j];
         }
     }
 
-    // 오른쪽 아래
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            m[i+1][j+1] += m[i][j];
+    // 오른쪽 대각선 아래
+    for(int i=0;i<=n;i++) {
+        for(int j=0;j<=n;j++) {
+            board[i+1][j+1] += board[i][j];
         }
     }
 
     int cnt=0;
-    for(int i=0;i<N;i++) {
-        for(int j=0;j<N;j++) {
-            if(m[i][j]) cnt++;
+    for(int i=0;i<=n;i++) {
+        for(int j=0;j<=n;j++) {
+            if(board[i][j]) cnt++;
         }
     }
     cout << cnt;
