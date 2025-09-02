baa4d9d70ad886783ebfb487dfefbc840bfcac17
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-02 23:12:38 +0900

    Bronze IV

--- 34xxx/34183. SUAPC 의자 준비하기/SUAPC 의자 준비하기.cc
+++ 34xxx/34183. SUAPC 의자 준비하기/SUAPC 의자 준비하기.cc
@@ -5,7 +5,6 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n, m, a, b; cin >> n >> m >> a >> b;
 
-    int total=0;
-    if(n*3>m) total = (n*3-m)*a+b;
-    cout << total;
+    if(n*3>m) cout << (n*3-m)*a+b;
+    else cout << 0;
 }
