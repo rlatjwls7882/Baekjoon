e5ebac407397f568671d287f992f71482948e448
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-04 00:17:17 +0900

    Silver V

---
+++ 21xxx/21760. 야구 시즌/야구 시즌.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        int n, m, k, d; cin >> n >> m >> k >> d;
+        int cur = m*m + k*n*(m*(m-1)/2);
+        if(cur>d) cout << "-1\n";
+        else cout << d/cur*cur << '\n';
+    }
+}
