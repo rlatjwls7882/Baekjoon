29a1652d30fb965fbb777ee7150e2ad31f923174
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-15 10:50:01 +0900

    Silver I

---
+++ 01xxx/1629. 곱셈/곱셈.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll a, b, c;
+
+ll expo_pow(ll a, ll b) {
+    if(b==0) return 1;
+    if(b==1) return a%c;
+
+    ll tmp = expo_pow(a, b/2);
+    if(b%2) return tmp*tmp%c*a%c;
+    return tmp*tmp%c;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> a >> b >> c;
+    cout << expo_pow(a, b);
+}
