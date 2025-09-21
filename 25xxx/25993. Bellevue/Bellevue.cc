e5b4b0e51d6cee079922910acd5036374d5d0545
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-21 15:40:06 +0900

    Silver I

--- 25xxx/25993. Bellevue/Bellevue.cc
+++ 25xxx/25993. Bellevue/Bellevue.cc
@@ -6,12 +6,8 @@ typedef pair<ld, ld> pdd;
 
 const long double PI = atanl(1)*4;
 
-ld getDist(pdd a, pdd b) {
-    return sqrtl((b.first-a.first)*(b.first-a.first) + b.second*b.second);
-}
-
-ld getAngle(pdd a, pdd b) {
-    return 90-asinl(abs(b.first-a.first)/getDist(a, b))*180/PI;
+ld getAngle(pdd a, pdd b, pdd c) {
+    return atanl(b.second/min(b.first-a.first, c.first-b.first))*180/PI;
 }
 
 int main() {
@@ -21,6 +17,6 @@ int main() {
     for(int i=0;i<n;i++) cin >> v[i].first >> v[i].second;
 
     ld res=0;
-    for(int i=1;i<n-1;i++) res = max({res, getAngle(v[0], v[i]), getAngle(v[n-1], v[i])});
-    cout << fixed << setprecision(7) << res;
+    for(int i=1;i<n-1;i++) res = max(res, getAngle(v[0], v[i], v[n-1]));
+    cout << fixed << setprecision(6) << res;
 }
