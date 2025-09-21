f4f616049ac4231e9233dff4168e71d9ba9882eb
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-21 15:41:08 +0900

    Silver I

--- 25xxx/25993. Bellevue/Bellevue.cc
+++ 25xxx/25993. Bellevue/Bellevue.cc
@@ -6,10 +6,6 @@ typedef pair<ld, ld> pdd;
 
 const long double PI = atanl(1)*4;
 
-ld getAngle(pdd a, pdd b, pdd c) {
-    return atanl(b.second/min(b.first-a.first, c.first-b.first))*180/PI;
-}
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
@@ -17,6 +13,6 @@ int main() {
     for(int i=0;i<n;i++) cin >> v[i].first >> v[i].second;
 
     ld res=0;
-    for(int i=1;i<n-1;i++) res = max(res, getAngle(v[0], v[i], v[n-1]));
+    for(int i=1;i<n-1;i++) res = max(res, atanl(v[i].second/min(v[i].first-v[0].first, v[n-1].first-v[i].first))*180/PI);
     cout << fixed << setprecision(6) << res;
 }
