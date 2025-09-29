aff06e136dcf3a21a3baa2a54512517b4dc70c26
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-30 01:50:55 +0900

    Unrated

---
+++ 21xxx/21293. No Description 2/No Description 2.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int z, y, x, w, v, u, t, s, r, p, o, n, m, l, k, j, i, h, g, f, e, d, c, b, a; cin >> z >> y >> x >> w >> v >> u >> t >> s >> r >> p >> o >> n >> m >> l >> k >> j >> i >> h >> g >> f >> e >> d >> c >> b >> a;
+    cout << a-b << '\n' << a*b << '\n' << a+b+1;
+}
---
+++ 21xxx/21293. No Description 2/README.md
@@ -0,0 +1,16 @@
+# [Unrated] No Description 2
+
+[문제 링크](https://www.acmicpc.net/problem/21293) 
+
+## 문제 설명
+
+<p>이번엔 정말 아무 정보도 없습니다! 질문도 받지 않습니다! 예제도 없습니다!</p>
+
+## 입력 
+
+ Empty
+
+## 출력 
+
+ Empty
+
