ecef64d16f5b1a5d3ab2abe5dc793e2f8fa3e4b8
Seojin Kim <rlatjwls7882@gmail.com>
2025-09-30 01:52:20 +0900

    Unrated

--- 21xxx/21293. No Description 2/No Description 2.cc
+++ 21xxx/21293. No Description 2/No Description 2.cc
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int z, y, x, w, v, u, t, s, r, p, o, n, m, l, k, j, i, h, g, f, e, d, c, b, a; cin >> z >> y >> x >> w >> v >> u >> t >> s >> r >> p >> o >> n >> m >> l >> k >> j >> i >> h >> g >> f >> e >> d >> c >> b >> a;
