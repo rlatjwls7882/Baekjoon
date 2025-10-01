0e0f4fb7e10e8b29099aa2bc5d4f4e053f454266
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-01 21:35:30 +0900

    Silver II

--- 33xxx/33526. Anti－Fan Death/Anti－Fan Death.cc
+++ 33xxx/33526. Anti－Fan Death/Anti－Fan Death.cc
@@ -1,19 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string s = "ZNA";
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
-    if(n==1) {
-        cout << "ZNA\nNAZ\nAZN";
-    } else {
-        for(int i=0;i<3*n;i++) {
-            for(int j=0;j<n;j++) {
-                if(i<n) cout << "AZN";
-                else if(i<2*n) cout << "ZNA";
-                else cout << "NAZ";
-            }
-            cout << '\n';
+    for(int i=0;i<3*n;i++) {
+        for(int j=0;j<3*n;j++) {
+            if(j<n) cout << s[i/n];
+            else if(j<2*n) cout << s[(i/n+1)%3];
+            else cout << s[(i/n+2)%3];
         }
+        cout << '\n';
     }
 }
