7d935422b2c7d1b719ca6ea9f58e31dd0aa69c3f
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-10 21:44:41 +0900

    Bronze I

--- 33xxx/33556. Java String Equals/Java String Equals.cc
+++ 33xxx/33556. Java String Equals/Java String Equals.cc
@@ -1,12 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool equalsIgnoreCase(string a, string b) {
+bool equalIgnoreCase(string a, string b) {
     if(a.length()!=b.length() || b=="null") return false;
     for(int i=0;i<a.length();i++) {
-        if(a[i]>='a') a[i]-=32;
-        if(b[i]>='a') b[i]-=32;
-        if(a[i]!=b[i]) return false;
+        if(a[i]!=b[i] && a[i]-32!=b[i] && a[i]!=b[i]-32) return false;
     }
     return true;
 }
@@ -15,5 +13,5 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     string a, b; cin >> a >> b;
     if(a=="null") cout << "NullPointerException\nNullPointerException";
-    else cout << (a==b ? "true\n" : "false\n") << (equalsIgnoreCase(a, b) ? "true" : "false");
+    else cout << (a==b ? "true\n" : "false\n") << (equalIgnoreCase(a, b) ? "true" : "false");
 }
