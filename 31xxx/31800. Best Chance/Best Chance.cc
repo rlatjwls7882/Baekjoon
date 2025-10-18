aa6a438d248c4c487018e908897b18fad03bfcd1
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-18 22:27:40 +0900

    Bronze I

--- 31xxx/31800. Best Chance/Best Chance.cc
+++ 31xxx/31800. Best Chance/Best Chance.cc
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int profit[300'000], price[300'000];
+int profit[300'000];
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -13,7 +13,5 @@ int main() {
         if(profit[i]>max1) max2=max1, max1=profit[i];
         else if(profit[i]>max2) max2=profit[i];
     }
-    for(int i=0;i<n;i++) cin >> price[i];
-
-    for(int i=0;i<n;i++) cout << profit[i] - ((max1==profit[i] ? max2 : max1) - price[i]) - price[i] << ' ';
+    for(int i=0;i<n;i++) cout << profit[i] - (max1==profit[i] ? max2 : max1) << ' ';
 }
