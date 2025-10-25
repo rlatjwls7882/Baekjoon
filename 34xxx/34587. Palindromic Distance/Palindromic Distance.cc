21bc31730e70590b4260423adb656ab704b429fd
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-26 04:15:47 +0900

    Gold III

--- 34xxx/34587. Palindromic Distance/Palindromic Distance.cc
+++ 34xxx/34587. Palindromic Distance/Palindromic Distance.cc
@@ -4,12 +4,12 @@ using namespace std;
 string s;
 int dp[3000][3000]; // dp[L][R] : L부터 R까지 팰린드롬이도록 하는 최소 연산 수
 
-int chk(int l, int r) {
+int dfs(int l, int r) {
     if(l>=r) return 0;
     if(dp[l][r]!=-1) return dp[l][r];
 
-    if(s[l]==s[r]) dp[l][r] = chk(l+1, r-1);
-    else dp[l][r] = min({chk(l+1, r), chk(l, r-1), chk(l+1, r-1)})+1;
+    if(s[l]==s[r]) dp[l][r] = dfs(l+1, r-1);
+    else dp[l][r] = min({dfs(l+1, r), dfs(l, r-1), dfs(l+1, r-1)})+1;
     return dp[l][r];
 }
 
@@ -18,7 +18,7 @@ int main() {
     int t; cin >> t;
     while(t--) {
         cin >> s;
-        memset(dp, -1, sizeof dp);
-        cout << chk(0, s.length()-1) << '\n';
+        for(int i=0;i<s.length();i++) for(int j=i+1;j<s.length();j++) dp[i][j]=-1;
+        cout << dfs(0, s.length()-1) << '\n';
     }
 }
