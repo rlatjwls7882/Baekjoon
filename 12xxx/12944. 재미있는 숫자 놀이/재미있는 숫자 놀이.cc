7ebcc7bfea7ffb674587f6ddaf2e0d7792a677e9
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-27 05:52:29 +0900

    Gold II

---
+++ 12xxx/12944. 재미있는 숫자 놀이/재미있는 숫자 놀이.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll n, k, cnt, a[20];
+
+void back(int depth=0, int start=0, ll val=1) {
+    if(val>n) return;
+    if(depth) {
+        if(depth%2) cnt += n/val;
+        else cnt -= n/val;
+    }
+    for(int i=start;i<k;i++) back(depth+1, i+1, lcm(val, a[i]));
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> k;
+    for(int i=0;i<k;i++) cin >> a[i];
+    back();
+    cout << cnt;
+}
