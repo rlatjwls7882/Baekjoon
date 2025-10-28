217c69a17c94181ac0a68bd17d99b902e3f2249d
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-28 14:00:30 +0900

    Gold IV

---
+++ 07xxx/7140. 데이터 만들기 1/데이터 만들기 1.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cout << "103\n";
+	for(int i=0;i<103;i++) cout << "0\n";
+	cout << "1\n1 2";
+}
