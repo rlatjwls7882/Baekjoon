769b24e53abe313129fc662f67a21379a5ad2100
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-28 13:54:58 +0900

    Platinum V

---
+++ 07xxx/7147. 데이터 만들기 8/데이터 만들기 8.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cout << "902 1501\n";
+	for(int i=0;i<901;i++) cout << i << ' ' << i+1 << '\n';
+	for(int i=0;i<600;i++) cout << i << ' ' << i+3 << '\n';
+}
