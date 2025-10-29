1abe75ac6b1a9e69dcdfdfc0710890b9da73d61b
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-29 11:37:04 +0900

    Bronze II

--- 28xxx/28786. Выстрел в голову/Выстрел в голову.cc
+++ 28xxx/28786. Выстрел в голову/Выстрел в голову.cc
@@ -5,20 +5,14 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n, m, a, b; cin >> n >> m >> a >> b;
 
-    int ammo=0, t=0;
-    while(n) {
-        if(!ammo) {
-            if(b*min(m, n)<=a) {
-                ammo++;
-                t+=b;
-            } else {
-                ammo=m;
-                t+=a;
-            }
-        } else {
-            ammo--;
+    int t=n;
+    while(n>0) {
+        if(b*min(m, n)<=a) {
             n--;
-            t++;
+            t+=b;
+        } else {
+            n-=m;
+            t+=a;
         }
     }
     cout << t;
