8b5b91c88be5add4721a01110ebbc105c3ec4ce4
Seojin Kim <rlatjwls7882@gmail.com>
2025-10-30 13:29:05 +0900

    Silver II

--- 05xxx/5600. 품질검사/품질검사.cc
+++ 05xxx/5600. 품질검사/품질검사.cc
@@ -4,28 +4,25 @@ using namespace std;
 int res[301];
 
 struct element {
-    int i, j, k, r;
-    bool operator<(const element e) const {
-        return r>e.r;
-    }
+    int i, j, k;
 };
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int a, b, c, n; cin >> a >> b >> c >> n;
-    vector<element> v(n);
-    for(int i=0;i<n;i++) cin >> v[i].i >> v[i].j >> v[i].k >> v[i].r;
-    sort(v.begin(), v.end());
 
+    vector<element> v;
     fill(res, res+a+b+c+1, 2);
+    while(n--) {
+        int i, j, k, r; cin >> i >> j >> k >> r;
+        if(r) res[i]=res[j]=res[k]=1;
+        else v.push_back({i, j, k});
+    }
+
     for(auto e:v) {
-        if(e.r) {
-            res[e.i]=res[e.j]=res[e.k]=1;
-        } else {
-            if(res[e.i]==1 && res[e.j]==1) res[e.k]=0;
-            else if(res[e.i]==1 && res[e.k]==1) res[e.j]=0;
-            else if(res[e.j]==1 && res[e.k]==1) res[e.i]=0;
-        }
+        if(res[e.i]==1 && res[e.j]==1) res[e.k]=0;
+        else if(res[e.i]==1 && res[e.k]==1) res[e.j]=0;
+        else if(res[e.j]==1 && res[e.k]==1) res[e.i]=0;
     }
     for(int i=1;i<=a+b+c;i++) cout << res[i] << '\n';
 }
