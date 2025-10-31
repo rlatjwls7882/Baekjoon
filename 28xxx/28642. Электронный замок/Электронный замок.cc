4cc9a7daaf1fe53b5ff43f9d52a37aa401e37d04
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-01 05:42:36 +0900

    Bronze I

---
+++ 28xxx/28642. Электронный замок/Электронный замок.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    if(n%2) {
+        cout << 7;
+        n-=3;
+    }
+
+    while(n) {
+        cout << 1;
+        n-=2;
+    }
+}
