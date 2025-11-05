262a461a03d4b4ed8e21dc1eaee9a75b6cfabd5a
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-06 08:38:35 +0900

    Diamond V

---
+++ 14xxx/14180. 배열의 특징/배열의 특징.cc
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+const int MAX = 200'001;
+
+ll A[MAX], preSum[MAX];
+
+struct element {
+    ll a, b;
+    ld x=-1e300;
+};
+
+ld meetX(element a, element b) {
+    return (ld)(a.b-b.b)/(b.a-a.a);
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    for(int i=1;i<=n;i++) {
+        cin >> A[i];
+        preSum[i] = preSum[i-1]+A[i];
+    }
+
+    ll res=0;
+    vector<element> stk;
+    for(int i=1;i<=n;i++) {
+        ll x = A[i];
+        element cur = {i, -preSum[i-1]};
+
+        while(!stk.empty()) {
+            cur.x = meetX(cur, stk.back());
+            if(cur.x>stk.back().x) break;
+            stk.pop_back();
+        }
+        stk.push_back(cur);
+
+        int left=0, right=stk.size()-1;
+        while(left<right) {
+            int mid = left+right+1>>1;
+            if(x<stk[mid].x) right=mid-1;
+            else left=mid;
+        }
+        res = max(res, stk[left].a*x+stk[left].b + preSum[i-1]-i*A[i]);
+    }
+
+    stk.clear();
+    for(int i=n;i>=1;i--) {
+        ll x = -A[i];
+        element cur = {-i, -preSum[i]};
+
+        while(!stk.empty()) {
+            cur.x = meetX(cur, stk.back());
+            if(cur.x>stk.back().x) break;
+            stk.pop_back();
+        }
+        stk.push_back(cur);
+
+        int left=0, right=stk.size()-1;
+        while(left<right) {
+            int mid = left+right+1>>1;
+            if(x<stk[mid].x) right=mid-1;
+            else left=mid;
+        }
+        res = max(res, stk[left].a*x+stk[left].b + preSum[i]-i*A[i]);
+    }
+    for(int i=1;i<=n;i++) res += i*A[i];
+    cout << res;
+}
