f11037f498682e3ed1cbdcaf171e71f13f26fae9
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-12 11:40:45 +0900

    Gold II

--- 30xxx/30410. 접시 포개기/접시 포개기.cc
+++ 30xxx/30410. 접시 포개기/접시 포개기.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int blockMerge(int n) {
     int ret=1;
-    while(ret<=n) ret*=2;
+    while(ret<=n) ret<<=1;
     return ret;
 }
 
@@ -11,26 +11,17 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
 
-    vector<int> cnt;
-    int last2=0, last1=0;
+    int res=1, last2=0, last1=0;
     while(n--) {
         int cur; cin >> cur;
         if(cur==1) last1++;
         else last2++;
+        res = max(res, blockMerge(last2+last1/2));
         if(cur==2) {
-            last2+=last1/2;
-            if(last1%2) {
-                cnt.push_back(last2);
-                cnt.push_back(0);
-                last2 = last1/2;
-            }
+            if(last1%2) last2 = last1/2;
+            else last2 += last1/2;
             last1=0;
         }
     }
-    cnt.push_back(last2+last1/2);
-    cnt.push_back(0);
-
-    int res=1;
-    for(int i=0;i<cnt.size()-1;i++) res = max(res, blockMerge(cnt[i]+cnt[i+1]));
     cout << res;
 }
