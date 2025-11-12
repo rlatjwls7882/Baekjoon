e09ce3af087eaa7e15c5eeca47a4d0d9b2558372
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-12 10:25:36 +0900

    Silver I

--- 17xxx/17939. Gazzzua/Gazzzua.cc
+++ 17xxx/17939. Gazzzua/Gazzzua.cc
@@ -8,10 +8,10 @@ int main() {
     int n; cin >> n;
     for(int i=0;i<n;i++) cin >> a[i];
 
-    int sum=0, cur=a[n-1];
-    for(int i=n-2;i>=0;i--) {
-        sum += max(0, cur-a[i]);
+    int sum=0, cur=0;
+    for(int i=n-1;i>=0;i--) {
         cur = max(cur, a[i]);
+        sum += cur-a[i];
     }
     cout << sum;
 }
