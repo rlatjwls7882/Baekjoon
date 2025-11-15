46fba2e87d6b4c493297501ed8a9f406a5f8a757
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-15 12:29:53 +0900

    Platinum III

--- 34xxx/34738. Game of Pieces/Game of Pieces.cc
+++ 34xxx/34738. Game of Pieces/Game of Pieces.cc
@@ -6,66 +6,45 @@ const int MAX = 600'001*4;
 
 ll _size=1, segMin[MAX], segMax[MAX], lazyMin[MAX], lazyMax[MAX];
 
-void updateLazyMin(int nodeNum, int nodeL, int nodeR) {
+void updateLazy(int nodeNum, int nodeL, int nodeR) {
     if(lazyMin[nodeNum]) {
         segMin[nodeNum] += lazyMin[nodeNum];
+        segMax[nodeNum] += lazyMax[nodeNum];
         if(nodeL!=nodeR) {
             lazyMin[nodeNum*2] += lazyMin[nodeNum];
             lazyMin[nodeNum*2+1] += lazyMin[nodeNum];
+            lazyMax[nodeNum*2] += lazyMax[nodeNum];
+            lazyMax[nodeNum*2+1] += lazyMax[nodeNum];
         }
-        lazyMin[nodeNum]=0;
+        lazyMin[nodeNum]=lazyMax[nodeNum]=0;
     }
 }
 
-void updateRangeMin(int L, int R, ll val, int nodeNum=1, int nodeL=0, int nodeR=_size/2-1) {
-    updateLazyMin(nodeNum, nodeL, nodeR);
+void updateRange(int L, int R, ll val, int nodeNum=1, int nodeL=0, int nodeR=_size/2-1) {
+    updateLazy(nodeNum, nodeL, nodeR);
     if(R<nodeL || nodeR<L) return;
     if(L<=nodeL && nodeR<=R) {
-        lazyMin[nodeNum] += val;
-        updateLazyMin(nodeNum, nodeL, nodeR);
+        lazyMin[nodeNum] = lazyMax[nodeNum] = val;
+        updateLazy(nodeNum, nodeL, nodeR);
     } else {
         int mid = nodeL+nodeR>>1;
-        updateRangeMin(L, R, val, nodeNum*2, nodeL, mid);
-        updateRangeMin(L, R, val, nodeNum*2+1, mid+1, nodeR);
+        updateRange(L, R, val, nodeNum*2, nodeL, mid);
+        updateRange(L, R, val, nodeNum*2+1, mid+1, nodeR);
         segMin[nodeNum] = min(segMin[nodeNum*2], segMin[nodeNum*2+1]);
+        segMax[nodeNum] = max(segMax[nodeNum*2], segMax[nodeNum*2+1]);
     }
 }
 
 ll searchMin(int L, int R, int nodeNum=1, int nodeL=0, int nodeR=_size/2-1) {
-    updateLazyMin(nodeNum, nodeL, nodeR);
+    updateLazy(nodeNum, nodeL, nodeR);
     if(R<nodeL || nodeR<L) return LONG_LONG_MAX;
     if(L<=nodeL && nodeR<=R) return segMin[nodeNum];
     int mid = nodeL+nodeR>>1;
     return min(searchMin(L, R, nodeNum*2, nodeL, mid), searchMin(L, R, nodeNum*2+1, mid+1, nodeR));
 }
 
-void updateLazyMax(int nodeNum, int nodeL, int nodeR) {
-    if(lazyMax[nodeNum]) {
-        segMax[nodeNum] += lazyMax[nodeNum];
-        if(nodeL!=nodeR) {
-            lazyMax[nodeNum*2] += lazyMax[nodeNum];
-            lazyMax[nodeNum*2+1] += lazyMax[nodeNum];
-        }
-        lazyMax[nodeNum]=0;
-    }
-}
-
-void updateRangeMax(int L, int R, ll val, int nodeNum=1, int nodeL=0, int nodeR=_size/2-1) {
-    updateLazyMax(nodeNum, nodeL, nodeR);
-    if(R<nodeL || nodeR<L) return;
-    if(L<=nodeL && nodeR<=R) {
-        lazyMax[nodeNum] += val;
-        updateLazyMax(nodeNum, nodeL, nodeR);
-    } else {
-        int mid = nodeL+nodeR>>1;
-        updateRangeMax(L, R, val, nodeNum*2, nodeL, mid);
-        updateRangeMax(L, R, val, nodeNum*2+1, mid+1, nodeR);
-        segMax[nodeNum] = max(segMax[nodeNum*2], segMax[nodeNum*2+1]);
-    }
-}
-
 ll searchMax(int L, int R, int nodeNum=1, int nodeL=0, int nodeR=_size/2-1) {
-    updateLazyMax(nodeNum, nodeL, nodeR);
+    updateLazy(nodeNum, nodeL, nodeR);
     if(R<nodeL || nodeR<L) return 0;
     if(L<=nodeL && nodeR<=R) return segMax[nodeNum];
     int mid = nodeL+nodeR>>1;
@@ -100,16 +79,14 @@ int main() {
         int lo = lower_bound(vals.begin(), vals.end(), p)-vals.begin();
         if(ch=='|') {
             cout << 'S';
-            updateRangeMin(lo, lo, l);
-            updateRangeMax(lo, lo, l);
+            updateRange(lo, lo, l);
         } else {
             int hi = lower_bound(vals.begin(), vals.end(), p+l-1)-vals.begin();
             if(searchMin(lo, hi)!=searchMax(lo, hi)) {
                 cout << 'U';
             } else {
                 cout << 'S';
-                updateRangeMin(lo, hi, 1);
-                updateRangeMax(lo, hi, 1);
+                updateRange(lo, hi, 1);
             }
         }
     }
