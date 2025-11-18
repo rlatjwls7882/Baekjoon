be7522d4347ff3619dce9a84a4361967d8e512df
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-18 10:47:01 +0900

    Gold II

--- 24xxx/24537. 약수 계산/약수 계산.cc
+++ 24xxx/24537. 약수 계산/약수 계산.cc
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int cnt[1'000'001], gcdVal[1'000'001];
+const int MAX = 1'000'001;
+
+int cnt[MAX], dp[MAX];
+
+int solve(int k) {
+    if(dp[k]) return dp[k];
+
+    int gcdVal=-1;
+    for(int i=k;i<MAX;i+=k) {
+        if(cnt[i]) {
+            dp[k] += cnt[i];
+            if(gcdVal==-1) gcdVal=i;
+            else gcdVal = gcd(gcdVal, i);
+        }
+    }
+    if(gcdVal!=k) dp[k]=-1;
+    return dp[k]; 
+}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -11,27 +28,9 @@ int main() {
         cnt[cur]++;
     }
 
-    for(int i=1;i<=1'000'000;i++) {
-        gcdVal[i]=-1;
-        if(!cnt[i]) continue;
-        for(int j=1;j*j<=i;j++) {
-            if(i%j==0) {
-                if(j!=i) cnt[j] += cnt[i];
-                if(gcdVal[j]==-1) gcdVal[j]=i;
-                else gcdVal[j] = gcd(gcdVal[j], i);
-                if(j!=i/j) {
-                    if(j!=1) cnt[i/j] += cnt[i];
-                    if(gcdVal[i/j]==-1) gcdVal[i/j]=i;
-                    else gcdVal[i/j] = gcd(gcdVal[i/j], i);
-                }
-            }
-        }
-    }
-
     int q; cin >> q;
     while(q--) {
         int k; cin >> k;
-        if(gcdVal[k]!=k) cout << "-1\n";
-        else cout << cnt[k] << '\n';
+        cout << solve(k) << '\n';
     }
 }
