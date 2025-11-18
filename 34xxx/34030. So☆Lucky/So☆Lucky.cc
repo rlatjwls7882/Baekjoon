6dc157e137e730e648f5fffeb63773624e3368f8
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-18 18:47:25 +0900

    G5

--- 34xxx/34030. So☆Lucky/So☆Lucky.cc
+++ 34xxx/34030. So☆Lucky/So☆Lucky.cc
@@ -1,29 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a[200'000];
+int n, a[200'000];
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n; cin >> n;
-    for(int i=0;i<n;i++) cin >> a[i];
-
-    bool odd=true;
+bool isOdd() {
     int lastOdd=0, lastEven=0;
     for(int i=0;i<n;i++) {
         if(a[i]%2) {
-            if(lastOdd>a[i]) odd=false;
+            if(lastOdd>a[i]) return false;
             lastOdd=a[i];
         } else {
-            if(lastEven>a[i]) odd=false;
+            if(lastEven>a[i]) return false;
             lastEven=a[i];
         }
     }
-    bool even=true;
-    for(int i=0;i<n-1;i++) {
-        if(a[i]%2!=a[i+1]%2 && a[i]>a[i+1]) even=false;
-    }
+    return true;
+}
 
-    cout << (odd ? "So Lucky\n" : "Unlucky\n");
-    cout << (even ? "So Lucky" : "Unlucky");
+bool isEven() {
+    for(int i=0;i<n-1;i++) {
+        if(a[i]%2!=a[i+1]%2 && a[i]>a[i+1]) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    for(int i=0;i<n;i++) cin >> a[i];
+
+    cout << (isOdd() ? "So Lucky\n" : "Unlucky\n");
+    cout << (isEven() ? "So Lucky" : "Unlucky");
 }
