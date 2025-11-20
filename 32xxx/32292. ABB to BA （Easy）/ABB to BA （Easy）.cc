a5ad83b5e4ab36b20886e668fab3b014c17b3cfc
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-21 05:34:47 +0900

    Update ABB to BA （Easy）.cc

--- 32xxx/32292. ABB to BA （Easy）/ABB to BA （Easy）.cc
+++ 32xxx/32292. ABB to BA （Easy）/ABB to BA （Easy）.cc
@@ -6,22 +6,7 @@ int main() {
     int t; cin >> t;
     while(t--) {
         int n; string s; cin >> n >> s;
-        vector<char> stk;
-        for(char ch:s) {
-            if(ch=='A') {
-                stk.push_back('A');
-            } else {
-                int cnt=0;
-                while(stk.size()>=2 && stk[stk.size()-2]=='A' && stk[stk.size()-1]=='B') {
-                    cnt++;
-                    stk.pop_back();
-                    stk.pop_back();
-                }
-                stk.push_back('B');
-                while(cnt--) stk.push_back('A');
-            }
-        }
-        for(char ch:stk) cout << ch;
-        cout << '\n';
+        while(s.find("ABB")!=s.npos) s.replace(s.find("ABB"), 3, "BA");
+        cout << s << '\n';
     }
 }
