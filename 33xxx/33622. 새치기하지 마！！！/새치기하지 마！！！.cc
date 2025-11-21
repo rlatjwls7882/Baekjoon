7a6751637c1f8a453201321857ad2ff2bfd111a2
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-21 23:20:44 +0900

    Silver V

---
+++ 33xxx/33622. 새치기하지 마！！！/새치기하지 마！！！.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    cout << "2\n";
+    cout << n/2 << ' ' << n-n/2;
+}
