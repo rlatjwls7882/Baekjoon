4c3393091dd4fe663c1392634f9b2c81f63495fe
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-23 00:43:22 +0900

    Bronze III

--- 34xxx/34459. MIT Time/MIT Time.cc
+++ 34xxx/34459. MIT Time/MIT Time.cc
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int mit[13];
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    vector<int> mit; mit.push_back(5);
-    for(int i=0;i<12;i++) mit.push_back(mit.back()*5);
+    mit[0]=5;
+    for(int i=1;i<13;i++) mit[i] = mit[i-1]*5;
 
     int t; cin >> t;
     while(t--) {
         int n; cin >> n;
-        for(int i=0;i<mit.size();i++) {
+        for(int i=0;i<13;i++) {
             if(mit[i]>=n) {
                 cout << "MIT";
                 if(i) cout << "^" << i+1;
