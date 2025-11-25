b96b289f2db72b350bc54d7f56ffc98e9941f3ac
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-26 01:33:20 +0900

    Update update_README.py

--- update_README.py
+++ update_README.py
@@ -82,7 +82,7 @@ def get_header(handle):
   header += " [![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=rlatjwls3333)](https://solved.ac/rlatjwls3333/)\n"
   header += " ![mazandi profile](http://mazandi.herokuapp.com/api?handle=rlatjwls3333&theme=dark)\n"
   header += "#### [여기](https://github.com/Hiyabye/Baekjoon/blob/main/scripts/update_readme.py)에 있는 코드를 변경하여 자동으로 생성하였습니다.\n"
-  header += "#### 500KB까지 렌더링되기에 중간에 짤립니다. 전체 목록은 [여기에](./README.md)\n\n"
+  header += "#### 500KB까지 렌더링되기에 중간에 짤립니다. 전체 목록은 [여기에](./README.md)서 확인해주세요.\n\n"
   return header
 
 # README.md 테이블을 반환
