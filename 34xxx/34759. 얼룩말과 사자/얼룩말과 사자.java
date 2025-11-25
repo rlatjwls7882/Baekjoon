2712f1c4366fc91ae26f379362c2adf9d35befe3
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-26 00:31:18 +0900

    Bronze III

---
+++ 34xxx/34759. 얼룩말과 사자/얼룩말과 사자.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+	public static void main(String[] args) throws IOException {
+		long n = Long.valueOf(br.readLine());
+		bw.write(Long.toString(n*2));
+        bw.close();
+    } // end of main
+} // end of Main
