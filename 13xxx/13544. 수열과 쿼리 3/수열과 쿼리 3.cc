f27640aa2a9329ddee20c8e462e797ca9c3bd2f5
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-25 11:41:37 +0900

    Platinum III

--- 13xxx/13544. 수열과 쿼리 3/수열과 쿼리 3.cc
+++ 13xxx/13544. 수열과 쿼리 3/수열과 쿼리 3.cc
@@ -1,46 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MAX = 100'000;
+const int MAX = 100'001;
 
-int _size;
-vector<int> arr[MAX*4];
+int _size=1;
+vector<vector<int>> arr(MAX*4);
 
-void init() {
-    int n; cin >> n;
-    _size=1;
-    while(_size<n) _size<<=1;
-    _size<<=1;
-
-    for(int i=0;i<n;i++) {
-        int A; cin >> A;
-        arr[_size/2+i].push_back(A);
+int query(int L, int R, int val) {
+    int ret=0;
+    for(L+=_size, R+=_size;L<=R;L>>=1, R>>=1) {
+        if(L&1) ret += arr[L].end() - upper_bound(arr[L].begin(), arr[L].end(), val), L++;
+        if(!(R&1)) ret += arr[R].end() - upper_bound(arr[R].begin(), arr[R].end(), val), R--;
     }
-
-    for(int i=_size/2-1;i>0;i--) {
-        int l=0, r=0;
-        while(l<arr[i*2].size() || r<arr[i*2+1].size()) {
-            if(l==arr[i*2].size() || r<arr[i*2+1].size() && arr[i*2][l] > arr[i*2+1][r]) {
-                arr[i].push_back(arr[i*2+1][r++]);
-            } else {
-                arr[i].push_back(arr[i*2][l++]);
-            }
-        }
-    }
-}
-
-int getCnt(int L, int R, int nodeNum, int nodeL, int nodeR, int val) {
-    if(R<nodeL || nodeR<L) return 0;
-    if(L<=nodeL && nodeR<=R) {
-        return arr[nodeNum].end() - upper_bound(arr[nodeNum].begin(), arr[nodeNum].end(), val);
-    }
-    int mid = (nodeL+nodeR)/2;
-    return getCnt(L, R, nodeNum*2, nodeL, mid, val) + getCnt(L, R, nodeNum*2+1, mid+1, nodeR, val);
+    return ret;
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    init();
+    int n; cin >> n;
+    while(_size<n) _size<<=1;
+
+    for(int i=_size+1;i<=_size+n;i++) {
+        int A; cin >> A;
+        arr[i].push_back(A);
+    }
+
+    for(int i=_size-1;i>=1;i--) {
+        int l=0, r=0;
+        while(l<arr[i*2].size() || r<arr[i*2+1].size()) {
+            if(l==arr[i*2].size() || r<arr[i*2+1].size() && arr[i*2+1][r]<arr[i*2][l]) arr[i].push_back(arr[i*2+1][r++]);
+            else arr[i].push_back(arr[i*2][l++]);
+        }
+    }
 
     int lastAns=0;
     int m; cin >> m;
@@ -49,7 +40,7 @@ int main() {
         int i = a^lastAns;
         int j = b^lastAns;
         int k = c^lastAns;
-        lastAns = getCnt(i-1, j-1, 1, 0, _size/2-1, k);
+        lastAns = query(i, j, k);
         cout << lastAns << '\n';
     }
 }
