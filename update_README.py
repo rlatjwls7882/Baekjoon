e2ab26bb260f18122372a61b3101b652ae5f7202
Seojin Kim <rlatjwls7882@gmail.com>
2025-11-27 01:13:29 +0900

    Update update_README.py

--- update_README.py
+++ update_README.py
@@ -71,18 +71,18 @@ def get_solution_path(id):
   for file in files:
     if file[file.rfind('.'):] == '.md':
       continue
-    file = file.replace(" ", "%E2%80%85") # 링크에 띄어쓰기가 있는경우 
+    file = file.replace(" ", "%E2%80%85") # 링크에 띄어쓰기가 있는경우
     solution += f"[{ext[file[file.rfind('.'):]]}](./{file}) "
   return solution
 
-# README.md 헤더를 반환
+# README.md 헤더 반환
 def get_header(handle):
   header = '<div align="center">\n\n'
   header += "# Baekjoon\n\n"
   header += " [![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=rlatjwls3333)](https://solved.ac/rlatjwls3333/)\n"
   header += " ![mazandi profile](http://mazandi.herokuapp.com/api?handle=rlatjwls3333&theme=dark)\n"
   header += "#### [여기](https://github.com/Hiyabye/Baekjoon/blob/main/scripts/update_readme.py)에 있는 코드를 변경하여 자동으로 생성하였습니다.\n"
-  header += "#### 500KB까지 렌더링되기에 중간에 짤립니다. 전체 목록은 [여기에](./README.md)서 확인해주세요.\n\n"
+  header += "#### 500KB까지 렌더링되기에 중간에 짤립니다. 전체 목록은 [여기](./README.md)에서 확인해주세요.\n\n"
   return header
 
 # README.md 테이블을 반환
