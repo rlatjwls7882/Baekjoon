b685642fd49acad82d4622a02319cffbbf8a4eb1
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-05 10:22:40 +0900

    Gold III

--- 02xxx/2694. 합이 같은 구간/합이 같은 구간.cc
+++ 02xxx/2694. 합이 같은 구간/합이 같은 구간.cc
@@ -1,13 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a[10000];
+int m, a[10000];
+
+bool chk(int i) {
+    int last=0;
+    for(int j=0;j<m;j++) {
+        last+=a[j];
+        if(last==i) last=0;
+        else if(last>i) return false;
+    }
+    return chk && last==0;
+}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int t; cin >> t;
     while(t--) {
-        int m; cin >> m;
+        cin >> m;
         int sum=0;
         for(int i=0;i<m;i++) {
             cin >> a[i];
@@ -17,31 +27,8 @@ int main() {
         int res=sum;
         for(int i=1;i*i<sum;i++) {
             if(sum%i==0) {
-                bool chk=true;
-                int last=0;
-                for(int j=0;j<m;j++) {
-                    last+=a[j];
-                    if(last==i) {
-                        last=0;
-                    } else if(last>i) {
-                        chk=false;
-                        break;
-                    }
-                }
-                if(chk && last==0) res = min(res, i);
-
-                chk=true;
-                last=0;
-                for(int j=0;j<m;j++) {
-                    last+=a[j];
-                    if(last==sum/i) {
-                        last=0;
-                    } else if(last>sum/i) {
-                        chk=false;
-                        break;
-                    }
-                }
-                if(chk && last==0) res = min(res, sum/i);
+                if(chk(i)) res = min(res, i);
+                if(chk(sum/i)) res = min(res, sum/i);
             }
         }
         cout << res << '\n';
