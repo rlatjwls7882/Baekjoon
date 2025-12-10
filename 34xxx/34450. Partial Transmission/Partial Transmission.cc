b6c5385cd961781aea5bdd91ac3c4ddbf20f0d3f
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-10 17:15:26 +0900

    Bronze III

--- 34xxx/34450. Partial Transmission/Partial Transmission.cc
+++ 34xxx/34450. Partial Transmission/Partial Transmission.cc
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int s[100];
 bool chk[1101];
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, p; cin >> n >> p;
     for(int i=0;i<n-1;i++) {
-        cin >> s[i];
-        chk[s[i]]=true;
+        int s; cin >> s;
+        chk[s]=true;
     }
 
     for(int i=p;;i++) {
