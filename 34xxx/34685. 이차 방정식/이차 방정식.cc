334ba5f468a9b7ed9e817500e62e956842d4e830
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-16 23:01:47 +0900

    Gold II

--- 34xxx/34685. 이차 방정식/이차 방정식.cc
+++ 34xxx/34685. 이차 방정식/이차 방정식.cc
@@ -6,9 +6,10 @@ typedef long long ll;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     ll k; cin >> k;
+    ll boundary=abs(k)*2;
 
     unordered_set<ll> p;
-    for(ll a=-20'000'000;a<=20'000'000;a++) {
+    for(ll a=-boundary;a<=boundary;a++) {
         if(!(a+k)) continue;
         if(k*k%(a+k)==0) p.insert(-(a+k*k/(a+k)-k));
     }
