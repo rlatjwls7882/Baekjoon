931f9f2b2410950a56375082f4125c6c69765ce8
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-17 22:45:06 +0900

    Platinum V

--- 34xxx/34686. Rescue Squad/Rescue Squad.cc
+++ 34xxx/34686. Rescue Squad/Rescue Squad.cc
@@ -1,25 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int l[1001], conn[1001][1001], conn2[1001][1001];
-vector<pair<int, int>> edges;
+int l[1000], diag[1000][1000];
+vector<vector<int>> conn(1000);
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, m; cin >> n >> m;
-    for(int i=1;i<=n;i++) cin >> l[i];
+    for(int i=0;i<n;i++) cin >> l[i];
     while(m--) {
         int a, b; cin >> a >> b;
-        edges.push_back({min(a, b), max(a, b)});
-        conn[a][b]=conn[b][a]=true;
+        conn[a-1].push_back(b-1);
+        conn[b-1].push_back(a-1);
     }
 
     int res=-1;
-    for(auto [a, b]:edges) {
-        for(int c=a+1;c<=n;c++) {
-            if(conn[b][c]) {
-                if(conn2[a][c]) res = max(res, l[b]+conn2[a][c]);
-                conn2[a][c]=conn2[c][a] = max(conn2[a][c], l[a]+l[b]+l[c]);
+    for(int a=0;a<n;a++) {
+        for(int j=0;j<conn[a].size();j++) {
+            for(int k=j+1;k<conn[a].size();k++) {
+                int b = conn[a][j];
+                int c = conn[a][k];
+                if(diag[b][c]) res = max(res, l[a]+l[b]+l[c]+diag[b][c]);
+                diag[b][c]=diag[c][b] = max(diag[b][c], l[a]);
             }
         }
     }
