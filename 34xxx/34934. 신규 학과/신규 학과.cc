83515a6bd88f48c2026a33eb1e4c4cb013945184
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-22 07:49:40 +0900

    Bronze V

---
+++ 34xxx/34934. 신규 학과/신규 학과.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    while(n--) {
+        string s; int a; cin >> s >> a;
+        if(a==2026) {
+            cout << s;
+            break;
+        }
+    }
+}
