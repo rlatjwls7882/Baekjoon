6e589e282109dd8de211bba44956ba0591612d84
Seojin Kim <rlatjwls7882@gmail.com>
2025-12-25 00:17:35 +0900

    Unrated

--- 01xxx/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.java
+++ 01xxx/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.java
@@ -5,9 +5,8 @@ import java.io.OutputStreamWriter;
 public class Main {
     public static void main(String[] args) throws IOException {
     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	
-    	// 문제의 정답 출력
     	bw.write("문제의 정답");
     	bw.close();
     } // end of main
 } // end of Main class
+// M1rry Christmas!
