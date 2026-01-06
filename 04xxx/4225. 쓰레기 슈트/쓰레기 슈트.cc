ec5851ad27e115cb586c3f3bafdd0596f54a2e7e
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-06 10:42:46 +0900

    Platinum III

---
+++ 04xxx/4225. 쓰레기 슈트/쓰레기 슈트.cc
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ConvexHull {
+    using ll = long long;
+    struct point {
+        ll x, y, p=0, q=0;
+        bool operator<(const point o) const {
+            if(p*o.q!=o.p*q) return p*o.q>o.p*q;
+            if(y!=o.y) return y<o.y;
+            return x<o.x;
+        }
+        bool operator==(const point o) const {
+            return x==o.x && y==o.y;
+        }
+    };
+    vector<point> hull;
+
+    inline ll ccw(point a, point b, point c) {
+        point v1 = {b.x-a.x, b.y-a.y};
+        point v2 = {c.x-a.x, c.y-a.y};
+        return v1.x*v2.y-v2.x*v1.y;
+    }
+
+    inline ll ccw(point a, point b, point c, point d) {
+        point v1 = {b.x-a.x, b.y-a.y};
+        point v2 = {d.x-c.x, d.y-c.y};
+        return v1.x*v2.y-v2.x*v1.y;
+    }
+
+    inline ll dist(point a, point b) {
+        return (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y);
+    }
+
+    inline ll triArea(point a, point b, point c) { // area*2
+        return a.x*b.y+b.x*c.y+c.x*a.y-a.x*c.y-b.x*a.y-c.x*b.y;
+    }
+
+    inline ConvexHull(vector<point> v) {
+        for(int i=0;i<v.size();i++) v[i].p=v[i].q=0;
+        sort(v.begin(), v.end());
+
+        for(int i=1;i<v.size();i++) {
+            v[i].p = v[i].x-v[0].x;
+            v[i].q = v[i].y-v[0].y;
+        }
+        sort(v.begin(), v.end());
+
+        hull.clear();
+        for(point cur:v) {
+            while(hull.size()>=2 && ccw(hull[hull.size()-2], hull[hull.size()-1], cur)<=0) hull.pop_back();
+            hull.push_back(cur);
+        }
+    }
+
+    inline bool inConvexHull(point p) {
+        if(ccw(hull[0], hull[1], p)<0) return false;
+        if(ccw(hull[0], hull[hull.size()-1], p)>0) return false;
+
+        int l=1, r=hull.size()-2;
+        while(l<r) {
+            int m = l+r+1>>1;
+            if(ccw(hull[0], hull[m], p)>0) l=m;
+            else r=m-1;
+        }
+        return ccw(hull[l], hull[l+1], p)>=0;
+    }
+
+    inline ll rotating_calipers() {
+        ll ret=0, i=0, j=1;
+        while(i<hull.size() && j<hull.size()) {
+            ret = max(ret, dist(hull[i], hull[j]));
+            if(ccw(hull[i], hull[(i+1)%hull.size()], hull[j], hull[(j+1)%hull.size()])>0) j++;
+            else i++;
+        }
+        return ret;
+    }
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    for(int tc=1;;tc++) {
+        int n; cin >> n;
+        if(!n) break;
+        vector<ConvexHull::point> v(n);
+        for(int i=0;i<n;i++) cin >> v[i].x >> v[i].y;
+        ConvexHull ch(v);
+        auto &hull = ch.hull;
+
+        double res=INT_MAX;
+        for(int i=0;i<hull.size();i++) {
+            double cur=0;
+            for(int j=0;j<hull.size();j++) {
+                if(i!=j && (i+1)%hull.size()!=j) {
+                    cur = max(cur, ch.triArea(hull[i], hull[(i+1)%hull.size()], hull[j])/sqrt(ch.dist(hull[i], hull[(i+1)%hull.size()])));
+                }
+            }
+            res = min(res, cur);
+        }
+        cout << setprecision(2) << fixed << "Case " << tc << ": " << ceil(res*100-0.000000001)/100 << '\n';
+    }
+}
