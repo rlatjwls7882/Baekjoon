6b725b1c3b1eee4d0315fcad8939f855be714625
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-08 21:11:24 +0900

    Gold IV

--- 32xxx/32755. 차원의 나무 여행/차원의 나무 여행.cc
+++ 32xxx/32755. 차원의 나무 여행/차원의 나무 여행.cc
@@ -1,23 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int MAX = 501;
-
-bool vis[MAX];
-vector<vector<int>> conn(MAX);
-
-bool dfs(int cur, int depth) {
-    if(depth==4) return true;
-    vis[cur]=true;
-
-    bool ret=false;
-    for(int next:conn[cur]) {
-        if(!vis[next]) {
-            ret |= dfs(next, depth+1);
-        }
-    }
-    return ret;
-}
+vector<vector<int>> conn(501);
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -29,9 +13,7 @@ int main() {
     }
 
     for(int i=1;i<=n;i++) {
-        memset(vis, 0, sizeof vis);
-        if(dfs(i, 1)) return !(cout << n);
+        if(conn[i].size()==n-1) return !(cout << n-1);
     }
-
-    cout << n-1;
+    cout << n;
 }
