54596364b66118a9b225df1a5c031c16830696d8
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-14 12:15:46 +0900

    Bronze III

---
+++ 34xxx/34302. Win Streak/Win Streak.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acos(-1);
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+
+    int res=0, cur=0;
+    while(n--) {
+        int s, t; cin >> s >> t;
+        if(s>t) res = max(res, ++cur);
+        else cur=0;
+    }
+    cout << res;
+}
