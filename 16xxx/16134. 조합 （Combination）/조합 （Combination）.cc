758a6e499c431fe703d037cc643bded6caf06fd0
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-16 11:20:32 +0900

    Gold I

--- 16xxx/16134. 조합 （Combination）/조합 （Combination）.cc
+++ 16xxx/16134. 조합 （Combination）/조합 （Combination）.cc
@@ -1,29 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
+const ll MOD = 1e9+7;
 
-const ll MOD = 1000000007;
+ll fact[1'000'001];
 
-ll factDp[1000001] = {0, };
-ll fact(long long N) {
-    if(N<=1) return factDp[N]=1;
-    if(factDp[N]==0) {
-        factDp[N] = (fact(N-1)*N)%MOD;
+ll pow(ll a, ll b) {
+    ll ret=1;
+    while(b) {
+        if(b&1) ret=ret*a%MOD;
+        a=a*a%MOD;
+        b>>=1;
     }
-    return factDp[N];
+    return ret;
 }
 
-ll squareRev(long long N, long long K) {
-    if(K==0) return 1;
-    if(K==1) return N;
-
-    long long tmp = squareRev(N, K/2)%MOD;
-    if(K%2==1) return ((tmp*tmp)%MOD*N)%MOD;
-    else return (tmp*tmp)%MOD;
+ll comb(ll n, ll r) {
+    if(r<0 || r>n) return 0;
+    return fact[n]*pow(fact[n-r]*fact[r]%MOD, MOD-2)%MOD;
 }
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    long long N, R; cin >> N >> R;
-    cout << (fact(N)*squareRev(fact(R)*fact(N-R)%MOD, MOD-2))%MOD;
+    cin.tie(0)->sync_with_stdio(0);
+    int n, r; cin >> n >> r;
+    fact[0]=1;
+    for(int i=1;i<=n;i++) fact[i]=fact[i-1]*i%MOD;
+    cout << comb(n, r);
 }
