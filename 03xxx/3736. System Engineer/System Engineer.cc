c7194431e99a58f7e52cb2c27d474855baa6c528
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-19 16:26:34 +0900

    Platinum III

--- 03xxx/3736. System Engineer/System Engineer.cc
+++ 03xxx/3736. System Engineer/System Engineer.cc
@@ -9,88 +9,71 @@ typedef long double ld;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3f;
 const ld PI = acos(-1);
+const int MAX = 20000;
+const int S=0, E=MAX-1;
 
-struct Dinic { // O(V²E), 모든 간선의 용량이 1이면 O(min(V^(2/3), E^(1/2))E)
-    struct Edge {
-        int to, rev;
-        ll c;
-    };
-    const int S, E;
-    vector<int> level, work;
-    vector<vector<Edge>> conn;
+vector<vector<int>> conn(MAX);
+int n, a[MAX], b[MAX], level[MAX];
+bool used[MAX];
 
-    Dinic(int SZ, int s, int e):S(s), E(e) {
-        work = vector<int>(SZ);
-        level = vector<int>(SZ);
-        conn = vector<vector<Edge>>(SZ);
+void bfs() {
+    queue<int> q;
+    for(int i=0;i<n;i++) {
+        if(!used[i]) {
+            level[i]=0;
+            q.push(i);
+        } else {
+            level[i]=INF;
+        }
     }
-
-    void edge(int u, int v, ll cap) {
-        conn[u].push_back({v, (int)conn[v].size(), cap});
-        conn[v].push_back({u, (int)conn[u].size()-1, 0});
-    }
-
-    bool bfs() {
-        fill(level.begin(), level.end(), -1);
-        queue<int> q; q.push(S);
-        level[S]=0;
-        while(!q.empty()) {
-            int cur = q.front(); q.pop();
-            for(Edge &next:conn[cur]) {
-                if(level[next.to]==-1 && next.c) {
-                    level[next.to]=level[cur]+1;
-                    q.push(next.to);
-                }
+    while(!q.empty()) {
+        int cur = q.front(); q.pop();
+        for(int next:conn[cur]) {
+            if(b[next]!=-1 && level[b[next]]==INF) {
+                level[b[next]]=level[cur]+1;
+                q.push(b[next]);
             }
         }
-        return level[E]!=-1;
     }
+}
 
-    ll dfs(int cur, ll curFlow) {
-        if(cur==E) return curFlow;
-        for(int &i=work[cur];i<conn[cur].size();i++) {
-            Edge &next=conn[cur][i];
-            if(level[next.to]==level[cur]+1 && next.c) {
-                ll flow = dfs(next.to, min(curFlow, next.c));
-                if(flow) {
-                    next.c-=flow;
-                    conn[next.to][next.rev].c+=flow;
-                    return flow;
-                }
-            }
+bool dfs(int cur) {
+    for(int next:conn[cur]) {
+        if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
+            b[next]=cur;
+            a[cur]=next;
+            return true;
         }
-        return 0;
     }
-
-    ll res() {
-        ll total=0, flow;
-        while(bfs()) {
-            fill(work.begin(), work.end(), 0);
-            while(flow=dfs(S, LINF)) total+=flow;
-        }
-        return total;
-    }
-};
+    return false;
+}
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     while(true) {
-        int n; cin >> n;
+        cin >> n;
         if(cin.fail()) break;
 
-        Dinic dinic(2*n+2, 0, 2*n+1);
-        for(int i=1;i<=n;i++) {
-            dinic.edge(0, i, 1);
-            dinic.edge(i+n, 2*n+1, 1);
-        }
-        for(int i=1;i<=n;i++) {
+        for(int i=0;i<n;i++) conn[i].clear();
+        memset(used, 0, sizeof used);
+        for(int i=0;i<n;i++) {
             string s; cin >> s >> s;
             int k = stoi(s.substr(1, s.length()-2));
             while(k--) {
-                int conn; cin >> conn;
-                dinic.edge(i, conn+1, 1);
+                int next; cin >> next;
+                conn[i].push_back(next-n);
             }
         }
-        cout << dinic.res() << '\n';
+
+        memset(b, -1, sizeof b);
+        int res=0;
+        while(true) {
+            bfs();
+            int flow=0;
+            for(int i=0;i<n;i++) flow += !used[i] && dfs(i);
+            if(!flow) break;
+            res+=flow;
+        }
+        cout << res << '\n';
     }
 }
