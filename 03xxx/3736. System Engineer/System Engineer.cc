bdaa77cad68ff3cf3d1d2f5f09a3930a57337992
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-20 12:31:21 +0900

    Platinum III

--- 03xxx/3736. System Engineer/System Engineer.cc
+++ 03xxx/3736. System Engineer/System Engineer.cc
@@ -9,71 +9,80 @@ typedef long double ld;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3f;
 const ld PI = acos(-1);
-const int MAX = 20000;
-const int S=0, E=MAX-1;
 
-vector<vector<int>> conn(MAX);
-int n, a[MAX], b[MAX], level[MAX];
-bool used[MAX];
-
-void bfs() {
-    queue<int> q;
-    for(int i=0;i<n;i++) {
-        if(!used[i]) {
-            level[i]=0;
-            q.push(i);
-        } else {
-            level[i]=INF;
-        }
+struct HopcroftKarp {
+    const int SZ;
+    vector<unsigned char> used;
+    vector<int> a, b, level;
+    vector<vector<int>> conn;
+    HopcroftKarp(int aSZ, int bSZ=0):SZ(aSZ) {
+        used=vector<unsigned char>(SZ);
+        a=level=vector<int>(SZ);
+        b=vector<int>(max(bSZ, SZ), -1);
+        conn = vector<vector<int>>(SZ);
     }
-    while(!q.empty()) {
-        int cur = q.front(); q.pop();
-        for(int next:conn[cur]) {
-            if(b[next]!=-1 && level[b[next]]==INF) {
-                level[b[next]]=level[cur]+1;
-                q.push(b[next]);
+    void edge(int u, int v) {
+        conn[u].push_back(v);
+    }
+    void bfs() {
+        queue<int> q;
+        for(int i=0;i<SZ;i++) {
+            if(!used[i]) {
+                level[i]=0;
+                q.push(i);
+            } else {
+                level[i]=INF;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(b[next]!=-1 && level[b[next]]==INF) {
+                    level[b[next]]=level[cur]+1;
+                    q.push(b[next]);
+                }
             }
         }
     }
-}
-
-bool dfs(int cur) {
-    for(int next:conn[cur]) {
-        if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
-            b[next]=cur;
-            a[cur]=next;
-            return true;
+    bool dfs(int cur) {
+        for(int next:conn[cur]) {
+            if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
+                used[cur]=true;
+                b[next]=cur;
+                a[cur]=next;
+                return true;
+            }
         }
+        return false;
     }
-    return false;
-}
+    int res() {
+        int ret=0;
+        while(true) {
+            bfs();
+            int flow=0;
+            for(int i=0;i<SZ;i++) flow+=!used[i] && dfs(i);
+            if(!flow) break;
+            ret+=flow;
+        }
+        return ret;
+    }
+};
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     while(true) {
-        cin >> n;
+        int n; cin >> n;
         if(cin.fail()) break;
 
-        for(int i=0;i<n;i++) conn[i].clear();
-        memset(used, 0, sizeof used);
+        HopcroftKarp hk(n, 2*n);
         for(int i=0;i<n;i++) {
             string s; cin >> s >> s;
             int k = stoi(s.substr(1, s.length()-2));
             while(k--) {
                 int next; cin >> next;
-                conn[i].push_back(next-n);
+                hk.edge(i, next-n);
             }
         }
-
-        memset(b, -1, sizeof b);
-        int res=0;
-        while(true) {
-            bfs();
-            int flow=0;
-            for(int i=0;i<n;i++) flow += !used[i] && dfs(i);
-            if(!flow) break;
-            res+=flow;
-        }
-        cout << res << '\n';
+        cout << hk.res() << '\n';
     }
 }
