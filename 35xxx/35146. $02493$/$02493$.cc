1515307cff41e7b5ce4054a8d203218e3800129c
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-21 04:30:38 +0900

    Silver V

---
+++ 35xxx/35146. $02493$/$02493$.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acos(-1);
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t; cin >> t;
+    while(t--) {
+        int x; cin >> x;
+        cout << x << " 1 10 10\n";
+    }
+}
