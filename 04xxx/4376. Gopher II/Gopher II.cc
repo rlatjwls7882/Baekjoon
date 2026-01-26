b9fc500868e257b6657584620a89ed9a6006608c
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-27 01:09:45 +0900

    Platinum IV

---
+++ 04xxx/4376. Gopher II/Gopher II.cc
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+struct HopcroftKarp { // O(E√V)
+    const int aSZ, bSZ;
+    vector<int> a, b, level;
+    vector<vector<int>> conn;
+
+    HopcroftKarp(int _sz1, int _sz2):aSZ(_sz1), bSZ(_sz2) {
+        level = vector<int>(aSZ);
+        a = vector<int>(aSZ, -1);
+        b = vector<int>(bSZ, -1);
+        conn = vector<vector<int>>(aSZ);
+    }
+
+    void edge(int u, int v) {
+        conn[u].push_back(v);
+    }
+
+    void bfs() {
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                level[i]=0;
+                q.push(i);
+            } else {
+                level[i]=-1;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(b[next]!=-1 && level[b[next]]==-1) {
+                    level[b[next]]=level[cur]+1;
+                    q.push(b[next]);
+                }
+            }
+        }
+    }
+
+    bool dfs(int cur) {
+        for(int next:conn[cur]) {
+            if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
+                b[next]=cur;
+                a[cur]=next;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int res() {
+        int ret=0;
+        while(true) {
+            bfs();
+            int flow=0;
+            for(int i=0;i<aSZ;i++) flow+=a[i]==-1 && dfs(i);
+            if(!flow) break;
+            ret+=flow;
+        }
+        return ret;
+    }
+
+    vector<vector<int>> cover() {
+        auto visA = vector<unsigned char>(aSZ);
+        auto visB = vector<unsigned char>(bSZ);
+
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                q.push(i);
+                visA[i]=true;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(!visB[next] && b[next]!=-1 && !visA[b[next]]) {
+                    visB[next]=true;
+                    visA[b[next]]=true;
+                    q.push(b[next]);
+                }
+            }
+        }
+        vector<vector<int>> ret(2);
+        for(int i=0;i<aSZ;i++) if(!visA[i]) ret[0].push_back(i);
+        for(int i=0;i<bSZ;i++) if(visB[i]) ret[1].push_back(i);
+        return ret;
+    }
+};
+
+pair<ld, ld> b[100], a[100];
+
+ld sq(ld x) {
+    return x*x;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    while(true) {
+        ld n, m, s, v; cin >> n >> m >> s >> v;
+        if(cin.fail()) break;
+        for(int i=0;i<n;i++) cin >> a[i].first >> a[i].second;
+        for(int i=0;i<m;i++) cin >> b[i].first >> b[i].second;
+        HopcroftKarp hk(n, m);
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(sq(a[i].first-b[j].first)+sq(a[i].second-b[j].second)<=s*s*v*v) {
+                    hk.edge(i, j);
+                }
+            }
+        }
+        cout << n-hk.res() << '\n';
+    }
+}
