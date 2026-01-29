c743a7be7fd8268fec4debe75c9556b1ee301c10
Seojin Kim <rlatjwls7882@gmail.com>
2026-01-30 02:30:05 +0900

    Platinum IV

---
+++ 08xxx/8118. Board Covering/Board Covering.cc
@@ -0,0 +1,136 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+struct HopcroftKarp { // O(E√V)
+    int match=0;
+    const int aSZ, bSZ;
+    vector<int> a, b, level, work;
+    vector<vector<int>> conn;
+
+    HopcroftKarp(int _sz1, int _sz2):aSZ(_sz1), bSZ(_sz2) {
+        level = vector<int>(aSZ);
+        work = a = vector<int>(aSZ, -1);
+        b = vector<int>(bSZ, -1);
+        conn = vector<vector<int>>(aSZ);
+    }
+
+    void edge(int u, int v) {
+        conn[u].push_back(v);
+    }
+
+    void bfs() {
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                level[i]=0;
+                q.push(i);
+            } else {
+                level[i]=-1;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(b[next]!=-1 && level[b[next]]==-1) {
+                    level[b[next]]=level[cur]+1;
+                    q.push(b[next]);
+                }
+            }
+        }
+    }
+
+    bool dfs(int cur) {
+        for(int &i=work[cur];i<conn[cur].size();i++) {
+            int next=conn[cur][i];
+            if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
+                b[next]=cur;
+                a[cur]=next;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int res() {
+        while(true) {
+            bfs();
+            int flow=0;
+            fill(work.begin(), work.end(), 0);
+            for(int i=0;i<aSZ;i++) flow+=a[i]==-1 && dfs(i);
+            if(!flow) break;
+            match+=flow;
+        }
+        return match;
+    }
+
+    vector<vector<int>> cover() {
+        auto visA = vector<unsigned char>(aSZ);
+        auto visB = vector<unsigned char>(bSZ);
+
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                q.push(i);
+                visA[i]=true;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(!visB[next] && b[next]!=-1 && !visA[b[next]]) {
+                    visB[next]=true;
+                    visA[b[next]]=true;
+                    q.push(b[next]);
+                }
+            }
+        }
+        vector<vector<int>> ret(2);
+        for(int i=0;i<aSZ;i++) if(!visA[i]) ret[0].push_back(i);
+        for(int i=0;i<bSZ;i++) if(visB[i]) ret[1].push_back(i);
+        return ret;
+    }
+};
+
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    set<int> ban;
+    for(int i=0;i<3;i++) {
+        int a; cin >> a;
+        ban.insert(a);
+    }
+
+    HopcroftKarp hk(n*n, n*n);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            int cur = i*n+j;
+            if(cur%2 || ban.count(cur+1)) continue;
+            for(int k=0;k<4;k++) {
+                int nx = i+dx[k];
+                int ny = j+dy[k];
+                int next = nx*n+ny;
+                if(nx<0 || nx>=n || ny<0 || ny>=n || ban.count(next+1)) continue;
+                hk.edge(cur, next);
+            }
+        }
+    }
+    if(hk.res()<(n*n-3)/2) return !(cout << "NIE");
+    for(int i=0;i<n*n;i++) {
+        if(hk.a[i]!=-1) cout << i+1 << ' ' << hk.a[i]+1 << '\n';
+    }
+}
---
+++ 08xxx/8118. Board Covering/README.md
@@ -0,0 +1,37 @@
+# [Platinum IV] Board Covering
+
+[문제 링크](https://www.acmicpc.net/problem/8118) 
+
+## 문제 설명
+
+<p>We are given a board of n × n squares, where n is an odd integer meeting the inequality 3 ≤ n < 50, and a set of k = (n<sup>2</sup> - 3)/2 stones. Each stone has the shape of a rectangle covering exactly 2 squares of the board. The board squares are numbered consecutively by rows - the squares in the first row (from left to right) are numbered from 1 to n, the squares in the second row from n+1 to 2n, and so on until we reach the bottom right corner square whose number is n<sup>2</sup>.</p>
+
+<p>We cut any three squares out of the board, and next we want to cover it with the stones. We require that every square not removed should be covered with exactly one stone (covering also one adjacent square) and that cut out squares remain uncovered.</p>
+
+<p>Is it always possible?</p>
+
+<p>Write a program that:</p>
+
+<ul>
+	<li>reads from the standard input the dimension of the board n and numbers of three board squares that have been cut out,</li>
+	<li>examines whether such a board can be covered with stones. If not, the program writes the answer <code>NIE</code> ("<i>no</i>") in the standard output.<br>
+	If so, it writes in the standard output a sequence of k = (n<sup>2</sup> - 3)/2 pairs of integers indicating the positions of stones covering the given board.</li>
+</ul>
+
+<p>If there are many ways to cover the board with stones, your program should write only one (arbitrary) of them.</p>
+
+## 입력 
+
+ <p>In the only line of the standard input there are written four numbers separated by single spaces. The first number is the dimension of the board n, and the three other are the numbers of the cut out squares. The last number is followed by the end of the line.</p>
+
+<p>The data in the standard input are written correctly, and your program need not verify that.</p>
+
+## 출력 
+
+ <p>The standard output should contain:</p>
+
+<ul>
+	<li>either one word <code>NIE</code></li>
+	<li>or in each of k consecutive lines two numbers separated by a space, i.e. the numbers of two adjacent squares covered with one stone.</li>
+</ul>
+
