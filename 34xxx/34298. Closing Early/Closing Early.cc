02ebd3ffd12ffe1ebe69fc0361328575114d1a06
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-01 04:20:18 +0900

    Bronze III

---
+++ 34xxx/34298. Closing Early/Closing Early.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef long double ld;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    ll r, s, n; cin >> r >> s >> n;
+
+    if(!r) return !(cout << 0);
+    for(int i=1;i<=n;i++) {
+        ll cur; cin >> cur;
+        r=(r-cur+s)%s;
+        if(!r) return !(cout << i);
+    }
+    cout << -1;
+}
