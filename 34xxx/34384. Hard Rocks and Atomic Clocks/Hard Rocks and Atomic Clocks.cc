f8bf8ce95c1e1908e2d0d17296bc4bdbd89fcb50
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-06 21:11:53 +0900

    Bronze III

---
+++ 34xxx/34384. Hard Rocks and Atomic Clocks/Hard Rocks and Atomic Clocks.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    cout << 60-n/60%60;
+}
