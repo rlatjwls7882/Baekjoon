f82563aa598d353301e299057e82a35f90410989
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-07 15:50:50 +0900

    Platinum II

--- 13xxx/13263. 나무 자르기/나무 자르기.cc
+++ 13xxx/13263. 나무 자르기/나무 자르기.cc
@@ -2,17 +2,18 @@
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
 const int MAX = 100'001;
 
 ll a[MAX], b[MAX], dp[MAX];
 
 struct element {
     ll a, b;
-    double x=0;
+    ld x=1e-150;
 };
 
-double meetX(element a, element b) {
-    return (double)(a.b-b.b)/(b.a-a.a);
+ld meetX(element a, element b) {
+    return (ld)(a.b-b.b)/(b.a-a.a);
 }
 
 int main() {
@@ -21,20 +22,24 @@ int main() {
     for(int i=1;i<=n;i++) cin >> a[i];
     for(int i=1;i<=n;i++) cin >> b[i];
 
-    int left=0;
     vector<element> stk;
     for(int i=2;i<=n;i++) {
+        ll x = a[i];
         element cur = {b[i-1], dp[i-1]};
         while(!stk.empty()) {
             cur.x = meetX(stk.back(), cur);
             if(cur.x>stk.back().x) break;
             stk.pop_back();
         }
-        left = min(left, (int)stk.size()-1);
         stk.push_back(cur);
 
-        while(left+1<stk.size() && a[i]>stk[left+1].x) left++;
-        dp[i] = stk[left].a*a[i] + stk[left].b;
+        int l=0, r=stk.size()-1;
+        while(l<r) {
+            int mid = l+r+1>>1;
+            if(x<=stk[mid].x) r=mid-1;
+            else l=mid;
+        }
+        dp[i] = stk[l].a*x + stk[l].b;
     }
     cout << dp[n];
 }
