37d65dcd009857d85a5d3d7420c69f510db75f46
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-13 03:34:14 +0900

    Diamond IV

---
+++ 13xxx/13443. 바둑/바둑.cc
@@ -0,0 +1,170 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+struct HopcroftKarp { // O(E√V)
+    int match=0;
+    const int aSZ, bSZ;
+    vector<int> a, b, level, work;
+    vector<vector<int>> conn;
+
+    HopcroftKarp(int _sz1, int _sz2):aSZ(_sz1), bSZ(_sz2) {
+        level = vector<int>(aSZ);
+        work = a = vector<int>(aSZ, -1);
+        b = vector<int>(bSZ, -1);
+        conn = vector<vector<int>>(aSZ);
+    }
+
+    void edge(int u, int v) {
+        conn[u].push_back(v);
+    }
+
+    void bfs() {
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                level[i]=0;
+                q.push(i);
+            } else {
+                level[i]=-1;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(b[next]!=-1 && level[b[next]]==-1) {
+                    level[b[next]]=level[cur]+1;
+                    q.push(b[next]);
+                }
+            }
+        }
+    }
+
+    bool dfs(int cur) {
+        for(int &i=work[cur];i<conn[cur].size();i++) {
+            int next=conn[cur][i];
+            if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
+                b[next]=cur;
+                a[cur]=next;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int res() {
+        while(true) {
+            bfs();
+            int flow=0;
+            fill(work.begin(), work.end(), 0);
+            for(int i=0;i<aSZ;i++) flow+=a[i]==-1 && dfs(i);
+            if(!flow) break;
+            match+=flow;
+        }
+        return match;
+    }
+
+    bool isUnique() {
+        vector<int> inDegree(aSZ+bSZ);
+        vector<vector<int>> dag(aSZ+bSZ);
+        for(int cur=0;cur<aSZ;cur++) {
+            for(int next:conn[cur]) {
+                if(a[cur]==next) {
+                    dag[aSZ+next].push_back(cur);
+                    inDegree[cur]++;
+                } else {
+                    dag[cur].push_back(aSZ+next);
+                    inDegree[aSZ+next]++;
+                }
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<aSZ+bSZ;i++) {
+            if(!inDegree[i]) q.push(i);
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:dag[cur]) {
+                if(!(--inDegree[next])) q.push(next);
+            }
+        }
+        for(int i=0;i<aSZ+bSZ;i++) {
+            if(inDegree[i]) return false;
+        }
+        return true;
+    }
+
+    vector<vector<int>> cover() {
+        auto visA = vector<unsigned char>(aSZ);
+        auto visB = vector<unsigned char>(bSZ);
+
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                q.push(i);
+                visA[i]=true;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(!visB[next] && b[next]!=-1 && !visA[b[next]]) {
+                    visB[next]=true;
+                    visA[b[next]]=true;
+                    q.push(b[next]);
+                }
+            }
+        }
+        vector<vector<int>> ret(2);
+        for(int i=0;i<aSZ;i++) if(!visA[i]) ret[0].push_back(i);
+        for(int i=0;i<bSZ;i++) if(visB[i]) ret[1].push_back(i);
+        return ret;
+    }
+};
+
+string s[50];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    for(int i=0;i<n;i++) cin >> s[i];
+
+    int cnt=0;
+    HopcroftKarp hk(n*n, n*n);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cnt+=s[i][j]=='o' || s[i][j]=='.';
+            if(s[i][j]=='o') {
+                for(int k=0;k<4;k++) {
+                    int nx=i+dx[k];
+                    int ny=j+dy[k];
+                    if(nx<0 || nx>=n || ny<0 || ny>=n || s[nx][ny]!='.') continue;
+                    hk.edge(i*n+j, nx*n+ny);
+                }
+            }
+        }
+    }
+    cout << cnt-hk.res();
+}
