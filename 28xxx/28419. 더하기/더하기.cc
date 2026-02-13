118f937c463a51e4dff12cc0a94ee19fa6e4c8bc
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-14 01:38:36 +0900

    Silver IV

---
+++ 28xxx/28419. 더하기/더하기.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+
+    ll odd=0, even=0;
+    for(int i=1;i<=n;i++) {
+        int a; cin >> a;
+        if(i%2) odd+=a;
+        else even+=a;
+    }
+    if(n==3 && odd>even) cout << -1;
+    else cout << abs(even-odd);
+}
