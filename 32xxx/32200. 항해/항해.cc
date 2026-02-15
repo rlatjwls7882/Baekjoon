923e1bfa428b24cc071abd09df139130f6aa00a0
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-16 08:41:41 +0900

    Silver V

---
+++ 32xxx/32200. 항해/항해.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, x, y; cin >> n >> x >> y;
+
+    ll a=0, b=0;
+    while(n--) {
+        int A; cin >> A;
+
+        int cnt=A/x;
+        A=A%x-cnt*(y-x);
+
+        a+=cnt;
+        if(A>0) b+=A;
+    }
+    cout << a << '\n' << b;
+}
