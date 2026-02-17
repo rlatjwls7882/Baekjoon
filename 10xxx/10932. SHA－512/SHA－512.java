1dfc88169610ed85cf517f01529c112207f27298
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-18 05:00:27 +0900

    Unrated

---
+++ 10xxx/10932. SHA－512/SHA－512.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.security.MessageDigest;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        String s = br.readLine();
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        byte[] hash = md.digest(s.getBytes());
+
+        for(byte e:hash) {
+            bw.write(String.format("%02x", e));
+        }
+
+        bw.close();
+    } // end of main
+} // end of Main
