71f6d203ff755d0637f7d8f66a95c20a5dc01a3b
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-20 20:36:28 +0900

    Platinum I

---
+++ 24xxx/24549. 신촌방위본부의 부대 배치/신촌방위본부의 부대 배치.cc
@@ -0,0 +1,194 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+struct HopcroftKarp { // O(E√V)
+    int match=0;
+    const int aSZ, bSZ;
+    vector<int> a, b, level, work;
+    vector<vector<int>> conn;
+
+    HopcroftKarp(int aSz, int bSz):aSZ(aSz), bSZ(bSz), level(aSZ), work(aSz), a(aSz, -1), b(bSz, -1), conn(aSz) {}
+
+    void edge(int u, int v) {
+        conn[u].push_back(v);
+    }
+
+    void bfs() {
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) level[i]=0, q.push(i);
+            else level[i]=-1;
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(b[next]!=-1 && level[b[next]]==-1) {
+                    level[b[next]]=level[cur]+1;
+                    q.push(b[next]);
+                }
+            }
+        }
+    }
+
+    bool dfs(int cur) {
+        for(int &i=work[cur];i<conn[cur].size();i++) {
+            int next=conn[cur][i];
+            if(b[next]==-1 || level[b[next]]==level[cur]+1 && dfs(b[next])) {
+                b[next]=cur;
+                a[cur]=next;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int res() {
+        while(true) {
+            bfs();
+            int flow=0;
+            fill(work.begin(), work.end(), 0);
+            for(int i=0;i<aSZ;i++) flow+=a[i]==-1 && dfs(i);
+            if(!flow) break;
+            match+=flow;
+        }
+        return match;
+    }
+
+    bool isUnique() {
+        vector<int> inDegree(aSZ+bSZ);
+        vector<vector<int>> dag(aSZ+bSZ);
+        for(int cur=0;cur<aSZ;cur++) {
+            for(int next:conn[cur]) {
+                if(a[cur]==next) {
+                    dag[aSZ+next].push_back(cur);
+                    inDegree[cur]++;
+                } else {
+                    dag[cur].push_back(aSZ+next);
+                    inDegree[aSZ+next]++;
+                }
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<aSZ+bSZ;i++) {
+            if(!inDegree[i]) q.push(i);
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:dag[cur]) {
+                if(!(--inDegree[next])) q.push(next);
+            }
+        }
+        for(int i=0;i<aSZ+bSZ;i++) {
+            if(inDegree[i]) return false;
+        }
+        return true;
+    }
+
+    vector<vector<int>> cover() {
+        auto visA = vector<unsigned char>(aSZ);
+        auto visB = vector<unsigned char>(bSZ);
+
+        queue<int> q;
+        for(int i=0;i<aSZ;i++) {
+            if(a[i]==-1) {
+                q.push(i);
+                visA[i]=true;
+            }
+        }
+        while(!q.empty()) {
+            int cur = q.front(); q.pop();
+            for(int next:conn[cur]) {
+                if(!visB[next] && b[next]!=-1 && !visA[b[next]]) {
+                    visB[next]=true;
+                    visA[b[next]]=true;
+                    q.push(b[next]);
+                }
+            }
+        }
+        vector<vector<int>> ret(2);
+        for(int i=0;i<aSZ;i++) if(!visA[i]) ret[0].push_back(i);
+        for(int i=0;i<bSZ;i++) if(visB[i]) ret[1].push_back(i);
+        return ret;
+    }
+};
+
+bool block[300][300];
+int dx[] = {-3, 3, -2, 2, -3, 3, -2, 2};
+int dy[] = {2, 2, 3, 3, -2, -2, -3, -3};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m, k; cin >> n >> m >> k;
+    while(k--) {
+        int x, y; cin >> x >> y;
+        x--; y--;
+        block[x][y]=true;
+        if(x-1>=0) block[x-1][y]=true;
+        if(x+1<n) block[x+1][y]=true;
+        if(y+1<m) block[x][y+1]=true;
+        for(int i=0;i<8;i++) {
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+            block[nx][ny]=true;
+        }
+    }
+
+    HopcroftKarp hk(n*m, n*m);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if((i+j)%2 && !block[i][j]) {
+                for(int k=0;k<8;k++) {
+                    int nx=i+dx[k];
+                    int ny=j+dy[k];
+                    if(nx<0 || nx>=n || ny<0 || ny>=m || block[nx][ny]) continue;
+                    hk.edge(i*m+j, nx*m+ny);
+                }
+            }
+        }
+    }
+
+    set<pii> res;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(!block[i][j]) {
+                res.insert({i+1, j+1});
+            }
+        }
+    }
+    hk.res();
+    auto cov = hk.cover();
+    for(auto c:cov) {
+        for(auto e:c) {
+            res.erase({e/m+1, e%m+1});
+        }
+    }
+
+    cout << res.size() << '\n';
+    for(auto e:res) cout << e.first << ' ' << e.second << '\n';
+}
