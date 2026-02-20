8625afe71b298d7da968da24c46086441cea3c01
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-21 03:00:04 +0900

    Silver IV

---
+++ 25xxx/25972. 도미노 무너트리기/도미노 무너트리기.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    vector<pii> v(n);
+    for(int i=0;i<n;i++) cin >> v[i].x >> v[i].y;
+    sort(all(v));
+
+    int res=0, last=0;
+    for(auto e:v) {
+        if(last<e.x) res++;
+        last=e.x+e.y;
+    }
+    cout << res;
+}
