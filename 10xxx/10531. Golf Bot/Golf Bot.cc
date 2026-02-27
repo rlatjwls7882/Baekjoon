8eaa3a0a7bb7abb3e6849bd4f3a5187f78bb7da5
Seojin Kim <rlatjwls7882@gmail.com>
2026-02-28 06:12:49 +0900

    Platinum I

---
+++ 10xxx/10531. Golf Bot/Golf Bot.cc
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+typedef complex<double> cpx;
+
+void fft(vector<cpx> &a, bool inv=false) {
+    int n=a.size();
+    for(int i=1, j=0;i<n;i++) {
+        int bit=n>>1;
+        while(j>=bit) j^=bit, bit>>=1;
+        j^=bit;
+        if(i<j) swap(a[i], a[j]);
+    }
+
+    for(int len=2;len<=n;len<<=1) {
+        ld ang = 2*PI/len * (inv ? -1 : 1);
+        cpx w(cos(ang), sin(ang));
+        for(int i=0;i<n;i+=len) {
+            cpx wp(1, 0);
+            for(int j=0;j<len/2;j++) {
+                cpx u=a[i+j];
+                cpx v=a[i+j+len/2]*wp;
+                a[i+j]=u+v;
+                a[i+j+len/2]=u-v;
+                wp*=w;
+            }
+        }
+    }
+    if(inv) for(int i=0;i<n;i++) a[i]/=n;
+}
+
+vector<ll> mult(vector<ll> &_a, vector<ll> &_b) {
+    vector<cpx> a(all(_a)), b(all(_b));
+    int n=2;
+    while(n<a.size()+b.size()) n<<=1;
+    a.resize(n); b.resize(n);
+    fft(a); fft(b);
+
+    for(int i=0;i<n;i++) a[i]*=b[i];
+    fft(a, true);
+
+    vector<ll> ret(n);
+    for(int i=0;i<n;i++) ret[i]=llround(a[i].real());
+    return ret;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    vector<ll> a(200'001);
+    a[0]=1;
+    while(n--) {
+        int k; cin >> k;
+        a[k]=1;
+    }
+
+    int cnt=0;
+    auto ret = mult(a, a);
+    int m; cin >> m;
+    while(m--) {
+        int d; cin >> d;
+        if(ret[d]) cnt++;
+    }
+    cout << cnt;
+}
