4eb795a571144b045187768fc059b140ccdae922
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-02 14:40:46 +0900

    Silver III

---
+++ 35xxx/35296. 아침 점호/아침 점호.cc
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+string s[100];
+
+struct element {
+    int a, b, c;
+};
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    for(int i=0;i<n;i++) cin >> s[i];
+
+    vector<element> res;
+    if(n==1) {
+        for(int j=0;j<m;j++) {
+            if(s[0][j]=='X') {
+                if(j+1<m && s[0][j+1]=='X') {
+                    res.push_back({2, j+1, j+2});
+                    j++;
+                } else {
+                    res.push_back({1, j+1});
+                }
+            }
+        }
+    } else {
+        for(int j=0;j<m;j++) {
+            for(int i=0;i<n;i++) {
+                if(s[i][j]=='X') {
+                    if(i+1<n && s[i+1][j]=='X') {
+                        res.push_back({2, j*n+i+1, j*n+i+2});
+                        i++;
+                    } else {
+                        res.push_back({1, j*n+i+1});
+                    }
+                }
+            }
+        }
+    }
+
+    cout << res.size() << '\n';
+    for(auto e:res) {
+        cout << e.a << ' ' << e.b;
+        if(e.a==2) cout << ' ' << e.c;
+        cout << '\n';
+    }
+}
