6f03a00eba37637f8d53177086a4f3e99c15bd7b
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-04 20:00:44 +0900

    Silver IV

---
+++ 34xxx/34068. 오디션/오디션.cc
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+ll sq(ll x) { return x*x; }
+ld sq(ld x) { return x*x; }
+
+int sc[400'002], cur[200'001];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    sc[0]=n;
+    while(m--) {
+        int a, b; cin >> a >> b;
+        sc[cur[a]]--;
+        cur[a]++;
+        sc[cur[a]]++;
+    }
+
+    ll res=0;
+    for(int i=0;i<=2*n;i++) {
+        if(sc[i]>1) {
+            res+=sc[i]-1;
+            sc[i+1]+=sc[i]-1;
+        }
+    }
+    cout << res;
+}
