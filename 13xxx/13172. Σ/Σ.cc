7f8d50f08c2902a2b15854d32751e6ea237104be
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-18 15:00:10 +0900

    Gold IV

--- 13xxx/13172. Σ/Σ.cc
+++ 13xxx/13172. Σ/Σ.cc
@@ -1,31 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long MOD = 1000000007;
+typedef long long ll;
 
-long long expo_square(long long a, long long b) {
-    long long ret=1;
+const ll MOD = 1e9+7;
+
+ll exPow(ll a, ll b) {
+    ll ret=1;
     while(b>0) {
-        if(b%2) {
-            ret = ret*a%MOD;
-        }
+        if(b%2) ret = ret*a%MOD;
         a=a*a%MOD;
         b>>=1;
     }
     return ret;
 }
 
-int main(void) {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int M; cin >> M;
-    long long res=0;
-    while(M-->0) {
-        long long N, S; cin >> N >> S;
-        // S/N
-        long long gcdVal = gcd(N, S);
-        N /= gcdVal;
-        S /= gcdVal;
-        res = (res+S*expo_square(N, MOD-2)%MOD)%MOD;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int m; cin >> m;
+    ll res=0;
+    while(m-->0) {
+        ll n, s; cin >> n >> s;
+        res += s*exPow(n, MOD-2)%MOD;
     }
-    cout << res;
+    cout << res%MOD;
 }
