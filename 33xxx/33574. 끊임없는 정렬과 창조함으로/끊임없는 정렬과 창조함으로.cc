0c438a6ce16e241c4b6c0208cb184e4803528eed
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-21 01:15:47 +0900

    Silver IV

---
+++ 33xxx/33574. 끊임없는 정렬과 창조함으로/끊임없는 정렬과 창조함으로.cc
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    vector<int> v;
+    int q; cin >> q;
+    while(q--) {
+        int op, x, t; cin >> op >> x;
+        if(op==1) {
+            if(x==1) sort(all(v));
+            else sort(all(v), greater<int>());
+        } else {
+            cin >> t;
+            v.insert(v.begin()+x, t);
+        }
+    }
+    cout << sz(v) << '\n';
+    for(auto e:v) cout << e << ' ';
+}
