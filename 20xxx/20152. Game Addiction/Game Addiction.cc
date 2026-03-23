7a3d76520780436708443f9dfb7f61b04909546e
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-24 01:55:47 +0900

    Silver II

--- 20xxx/20152. Game Addiction/Game Addiction.cc
+++ 20xxx/20152. Game Addiction/Game Addiction.cc
@@ -41,8 +41,7 @@ int main() {
     for(int i=0;i<=n;i++) dp[i][0]=1;
 
     for(int i=1;i<=n;i++) {
-        for(int j=1;j<=n;j++) {
-            if(j>i) continue;
+        for(int j=1;j<=i;j++) {
             dp[i][j] = dp[i-1][j]+dp[i][j-1];
         }
     }
