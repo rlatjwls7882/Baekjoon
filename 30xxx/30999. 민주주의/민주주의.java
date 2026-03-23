8de9ac159d719d42dce1fb6e9d98d10452468188
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-24 01:26:37 +0900

    Bronze IV

---
+++ 30xxx/30999. 민주주의/민주주의.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		
+		// 문제 후보의 수 N, 출제위원의 수 M
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.valueOf(st.nextToken());
+		int M = Integer.valueOf(st.nextToken());
+		
+		// 출제될 문제의 수 계산
+		int cnt=0;
+		for(int i=0;i<N;i++) {
+			String string = br.readLine();
+			
+			int yes=0;
+			for(int j=0;j<M;j++) {
+				if(string.charAt(j)=='O') {
+					yes++;
+				}
+			}
+			
+			if(yes*2>=M) {
+				cnt++;
+			}
+		}
+		
+		bw.write(Integer.toString(cnt));
+		
+		bw.close();
+	} // end of main()
+} // end of Main class
