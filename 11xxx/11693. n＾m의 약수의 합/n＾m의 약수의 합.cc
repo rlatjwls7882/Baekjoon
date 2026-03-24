57f58c0f2ab6cc53efa87bb71cf81bc1ef0ed382
Seojin Kim <rlatjwls7882@gmail.com>
2026-03-24 14:49:24 +0900

    Gold I

---
+++ 11xxx/11693. n＾m의 약수의 합/n＾m의 약수의 합.cc
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+const ll MOD = 1e9+7;
+
+ll expo(ll n, ll k) {
+    ll ret=1;
+    while(k) {
+        if(k&1) ret=ret*n%MOD;
+        k>>=1;
+        n = n*n%MOD;
+    }
+    return ret;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    ll n, m; cin >> n >> m;
+    if(m==0) return !(cout << 1);
+
+    ll res=1;
+    for(ll i=2;i*i<=n;i++) {
+        if(n%i==0) {
+            ll cnt=0;
+            while(n%i==0) {
+                cnt++;
+                n/=i;
+            }
+            res=res*(expo(i, m*cnt+1)-1)%MOD*expo(i-1, MOD-2)%MOD;
+        }
+    }
+    if(n!=1) res = res*(expo(n, m+1)-1)%MOD*expo(n-1, MOD-2)%MOD;
+    cout << res%MOD;
+}
