c1004161b1986e54df0960dc7418f46ca2e876b0
Seojin Kim <rlatjwls7882@gmail.com>
2026-04-03 21:07:35 +0900

    Platinum III

---
+++ 01xxx/1126. 같은 탑/같은 탑.cc
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+const int MAX = 500'000;
+
+int dp[51][MAX*2+1]; // dp[][왼쪽 - 오른쪽 = j인 최대 높이]
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    memset(dp, -1, sizeof dp);
+    dp[0][MAX]=0;
+
+    for(int i=1;i<=n;i++) {
+        int h; cin >> h;
+        for(int j=0;j<=MAX*2;j++) {
+            if(dp[i-1][j]==-1) continue;
+            dp[i][j]=max(dp[i][j], dp[i-1][j]);
+            dp[i][j+h]=max(dp[i][j+h], dp[i-1][j]+h);
+            dp[i][j-h] = max(dp[i][j-h], dp[i-1][j]);
+        }
+    }
+    cout << (dp[n][MAX] ? dp[n][MAX] : -1);
+}
