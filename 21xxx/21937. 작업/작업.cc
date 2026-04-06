dfba69383e98f093d88a0f352d27197eba68f62f
Seojin Kim <rlatjwls7882@gmail.com>
2026-04-06 22:35:55 +0900

    Silver I

---
+++ 21xxx/21937. 작업/작업.cc
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+vvi dag(100'001);
+bool chk[100'001];
+
+void dfs(int cur) {
+    chk[cur]=true;
+    for(int next:dag[cur]) {
+        if(!chk[next]) dfs(next);
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    while(m--) {
+        int a, b; cin >> a >> b;
+        dag[b].push_back(a);
+    }
+    int x; cin >> x;
+    dfs(x);
+
+    cout << accumulate(chk, chk+n+1, -1);
+}
