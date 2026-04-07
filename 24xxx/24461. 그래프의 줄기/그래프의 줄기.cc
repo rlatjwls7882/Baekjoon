aaa8ce23533d180dd1ca0e78ba8059947e31ef59
Seojin Kim <rlatjwls7882@gmail.com>
2026-04-07 10:49:48 +0900

    Gold II

---
+++ 24xxx/24461. 그래프의 줄기/그래프의 줄기.cc
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/rope>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("fma")
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+#define x first
+#define y second
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define compress(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T> T sq(T x) { return x*x; }
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const ld PI = acosl(-1);
+const ld EPS = 1e-10;
+
+mt19937 rd((unsigned)chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> rnd_int(0, 0); // rnd_int(rd)
+uniform_real_distribution<double> rnd_real(0, 1); // rnd_real(rd)
+
+vvi conn(100'000);
+int inDegree[100'000], dead[100'000];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    for(int i=0;i<n-1;i++) {
+        int u, v; cin >> u >> v;
+        conn[u].push_back(v);
+        conn[v].push_back(u);
+        inDegree[u]++;
+        inDegree[v]++;
+    }
+
+    queue<int> q;
+    for(int i=0;i<n;i++) {
+        if(inDegree[i]==1) q.push(i);
+    }
+    while(sz(q)>2) {
+        int cnt=sz(q);
+        while(cnt--) {
+            int cur = q.front(); q.pop();
+            dead[cur]=true;
+            for(auto next:conn[cur]) {
+                if(--inDegree[next]==1) q.push(next);
+            }
+        }
+    }
+    for(int i=0;i<n;i++) {
+        if(!dead[i]) cout << i << ' ';
+    }
+}
