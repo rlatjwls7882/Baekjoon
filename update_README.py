8418339c993213a351a7d7cee92610471bcb95c0
Seojin Kim <rlatjwls7882@gmail.com>
2026-04-13 15:00:58 +0900

    Update update_README.py

--- update_README.py
+++ update_README.py
@@ -7,7 +7,7 @@ HANDLE = "rlatjwls3333"
 BASE_URL = "https://solved.ac/api/v3"
 COMMON_HEADERS = {
     "Accept": "application/json",
-    "User-Agent": "Mozilla/5.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 }
 
 # 공통 JSON 요청 함수
