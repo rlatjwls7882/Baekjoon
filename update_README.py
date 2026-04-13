13abff02b54f0966d499a111ab2e811542056693
Seojin Kim <rlatjwls7882@gmail.com>
2026-04-13 17:51:15 +0900

    Update update_README.py

--- update_README.py
+++ update_README.py
@@ -99,7 +99,7 @@ def get_header(handle):
     header += "# Baekjoon\n\n"
     header += " [![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=rlatjwls3333)](https://solved.ac/rlatjwls3333/)\n"
     header += " ![mazandi profile](http://mazandi.herokuapp.com/api?handle=rlatjwls3333&theme=dark)\n"
-    header += "#### [여기](https://github.com/Hiyabye/Baekjoon/blob/main/scripts/update_readme.py)에 있는 코드를 변경하여 자동으로 생성하였습니다.\n"
+    header += "#### [여기](https://github.com/Hiyabye/Baekjoon/blob/main/scripts/update_readme.py)에 있는 코드와 해외 Leetcode Repo를 참고하여 자동으로 생성하였습니다.\n"
     header += "#### 500KB까지 렌더링되기에 중간에 짤립니다. 전체 목록은 [여기](./README.md)에서 확인해주세요.\n\n"
     return header
 
