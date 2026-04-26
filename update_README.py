5a84bae9f7bcfacc9f163ca6d9e1bdcfc5566bea
Seojin Kim <rlatjwls7882@gmail.com>
2026-04-26 16:43:09 +0900

    Update update_README.py

--- update_README.py
+++ update_README.py
@@ -96,9 +96,9 @@ def get_solution_path(id):
 # README.md 헤더 반환
 def get_header(handle):
     header = "# Baekjoon Online Judge Solutions\n\n"
-    header += " ![user](https://github.com/user-attachments/assets/e33e7ac9-2ca3-4178-9dfc-2fde76cf47a3)\n"
-    header += " ![language](https://github.com/user-attachments/assets/9b4f6c2a-83ea-4207-b7d5-44786e1448f2)\n"
-    header += " ![solvedac](https://github.com/user-attachments/assets/5aaa4941-8005-432f-983e-afd972bb6d51)\n\n"
+    header += " ![user](https://github.com/user-attachments/assets/d75a8b5f-0580-4251-b707-64f3834e4af9)\n"
+    header += " ![language](https://github.com/user-attachments/assets/ccdf207d-72ad-4e48-84ea-5a1557248933)\n"
+    header += " ![solvedac](https://github.com/user-attachments/assets/438df4ac-42b2-4265-9af1-43e2cbf6161e)\n\n"
     header += "### 500KB까지 렌더링되기에 중간에 짤립니다. 전체 목록은 [여기](./README.md)에서 확인해주세요.\n\n"
     return header
 
